27ed9d99d6ff532c95bd2dbb65e717fce66ce088
gemvcnte <gemvicente6@gmail.com>
2023-11-18 15:39:00 +0800

    added the main server files

--- server/controller/teacherController.js
+++ server/controller/teacherController.js
@@ -1,40 +1,39 @@
-const { Teacher } = require("../models/userModel");
-const asyncHandler = require("express-async-handler");
-const generateAuthToken = require("../configs/auth");
-const bcryptjs = require("bcryptjs");
+const { Teacher } = require('../models/userModel')
+const asyncHandler = require('express-async-handler')
+const generateAuthToken = require('../configs/auth')
+const bcrypt = require('bcrypt')
 
 const teacherLogin = asyncHandler(async (req, res) => {
-  try {
-    const { username, password } = req.body;
+    try {
+        const { username, password } = req.body;
 
-    // find yung teacher base sa uname
-    const teacher = await Teacher.findOne({ username });
+        // find yung teacher base sa uname
+        const teacher = await Teacher.findOne({ username });
 
-    // check if the teacher exists
-    if (!teacher) {
-      return res.status(401).json({ message: "Invalid username or password" });
+        // check if the teacher exists
+        if (!teacher) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, teacher.password);
+
+        // Check if the passwords match
+        if (!passwordMatch) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+
+        const token = generateAuthToken(teacher)
+
+        res.json({ message: 'Login Successfully.', token });
+    } catch (error) {
+        console.error(`There is an error ${error}`);
+        return res.status(500).json({ message: 'Login failed.' });
     }
-
-    const passwordMatch = await bcryptjs.compare(password, teacher.password);
-
-    // Check if the passwords match
-    if (!passwordMatch) {
-      return res.status(401).json({ message: "Invalid username or password" });
-    }
-
-    const tokenPayload = {
-      _id: teacher.id,
-      username: teacher.username,
-      role: "teacher",
-    };
-
-    const token = generateAuthToken(tokenPayload);
-
-    res.json({ message: "Login Successfully.", token });
-  } catch (error) {
-    console.error(`There is an error ${error}`);
-    return res.status(500).json({ message: "Login failed." });
-  }
 });
 
-module.exports = { teacherLogin };
+
+
+module.exports = {
+  teacherLogin
+}
--- server/models/Announcement.js
+++ server/models/Announcement.js
@@ -10,6 +10,10 @@ const announcementSchema = mongoose.Schema({
         type: String,
         required: true,
     },
+    createdBy: {
+        type: String,
+        required: true,
+    },
 }, {timestamps: true})
 
 
