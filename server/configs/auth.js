593ce31f4bc29db9e1c9c6739d07cd0fd392e1c8
n0wickk <memetitv01@gmail.com>
2023-11-18 19:31:49 +0800

    added user role from tokenPayload

--- server/configs/auth.js
+++ server/configs/auth.js
@@ -1,14 +1,18 @@
 // auth.js
 
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 
 // Load environment variables from .env file
 dotenv.config();
 
 const generateAuthToken = (user) => {
-    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '12h' });
-    return token;
+  const token = jwt.sign(
+    { _id: user._id, username: user.username, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "12h" }
+  );
+  return token;
 };
 
 module.exports = generateAuthToken;
