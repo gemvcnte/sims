c7e393adc924eaf27988b009376c5175784be627
n0wickk <memetitv01@gmail.com>
2023-11-18 15:07:17 +0800

    removed client and server folder

--- client/src/components/layout/Sidebar/components/SidebarContainer/components/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as BackgroundCover } from "./BackgroundCover";
-export { default as CloseButton } from "./CloseButton";
-export { default as SidebarFooter } from "./SidebarFooter";
-export { default as SidebarHeader } from "./SidebarHeader";
--- client/src/components/layout/Sidebar/components/TeacherSidebar/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import SidebarContainer from "../SidebarContainer";
-
-export default function TeacherSidebar() {
-  return <SidebarContainer>teacher</SidebarContainer>;
-}
