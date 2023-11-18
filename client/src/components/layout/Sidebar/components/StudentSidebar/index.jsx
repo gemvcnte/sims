c2fa379e0fd9a64494c7ca7aee249e79e93bc366
gem <111511069+gemvcnte@users.noreply.github.com>
2023-11-18 15:40:26 +0800

    Delete client directory

--- client/src/components/layout/Sidebar/components/StudentSidebar/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import SidebarContainer from "../SidebarContainer";
-import { Link } from "react-router-dom";
-
-export default function StudentSidebar() {
-  return <SidebarContainer>test</SidebarContainer>;
-}
--- client/src/utils/BlankFieldNotification.jsx
+++
@@ -1,9 +0,0 @@
-import { toast } from "react-toastify";
-
-const notify = () => {
-  toast.warn("Please fill in all required fields (*)", {
-    theme: "light",
-  });
-};
-
-export default notify;
