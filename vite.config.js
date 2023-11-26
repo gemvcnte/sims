2556859ec61bd21bc95c7302b00fc2bdc076ab4f
n0wickk <memetitv01@gmail.com>
2023-11-26 09:54:02 +0800

    add shadcn

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),
       "@src": "/src",
       "@components": "/src/components",
       "@config": "/src/config",
