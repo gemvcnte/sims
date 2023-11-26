ff02bbc0076c032fa8c5d952219f435cdd9aa9d6
n0wickk <memetitv01@gmail.com>
2023-11-26 21:05:58 +0800

    Refactor SidebarFooter to Dynamically Display User Information
    Introduce user role and username display based on JWT token, improving
    sidebar footer with personalized user details. This provides a more
    tailored user experience.

--- src/components/layout/Sidebar/components/SidebarContainer/components/SidebarFooter/index.jsx
+++ src/components/layout/Sidebar/components/SidebarContainer/components/SidebarFooter/index.jsx
@@ -1,8 +1,29 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SidebarFooterDropdown } from "./SidebarFooterDropdown";
+import { jwtDecode } from "jwt-decode";
 
 export default function SidebarFooter() {
+  const [userRole, setUserRole] = useState(null);
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    const authToken = localStorage.getItem("authToken");
+
+    if (authToken) {
+      try {
+        const decodedToken = jwtDecode(authToken);
+        const role = decodedToken.role;
+        const username = decodedToken.username;
+
+        setUserRole(role);
+        setUsername(username);
+      } catch (error) {
+        console.error("Error decoding token:", error);
+      }
+    }
+  }, []);
+
   return (
     <Button
       variant="ghost"
@@ -11,8 +32,8 @@ export default function SidebarFooter() {
       <section className="flex items-center gap-2">
         <div className="h-10 w-10 rounded-full bg-foreground"></div>
         <div className="flex flex-col items-start justify-around">
-          <span>Firstname Lastname</span>
-          <span className="text-muted-foreground">role</span>
+          <span>{username || "Firstname Lastname"}</span>
+          <span className="text-muted-foreground">{userRole}</span>
         </div>
       </section>
       <section>
