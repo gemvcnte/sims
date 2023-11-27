bd70c669b13865e128192303a83ebb9401c86975
n0wickk <memetitv01@gmail.com>
2023-11-27 12:31:58 +0800

    Refactor StudentSidebar link and add StudentProfile route
    - Update StudentSidebar link to point to Profile page
    - Introduce new route for displaying student profile
    - Enhances user navigation and provides access to profile page

---
+++ src/pages/Student/StudentProfile/index.jsx
@@ -0,0 +1,10 @@
+import Topbar from "@/components/layout/Topbar";
+import React from "react";
+
+export default function StudentProfile() {
+  return (
+    <main className="w-full">
+      <Topbar>PROFILE</Topbar>
+    </main>
+  );
+}
