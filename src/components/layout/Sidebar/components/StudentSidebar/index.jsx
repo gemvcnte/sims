8c10f24e52874222ea57da50daff26c1033392f4
n0wickk <memetitv01@gmail.com>
2023-11-27 10:11:08 +0800

    Refactor sidebar components to use shared SidebarItem component
    Refactor StudentSidebar and TeacherSidebar components to use a shared
    SidebarItem component, eliminating code duplication and promoting
    consistency in the sidebar layout. This makes it easier to maintain and
    update sidebar items in the future. No associated issues.

--- src/components/layout/Sidebar/components/StudentSidebar/index.jsx
+++ src/components/layout/Sidebar/components/StudentSidebar/index.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 import SidebarContainer from "../SidebarContainer";
-import { Link } from "react-router-dom";
+import SidebarItem from "@/components/layout/Sidebar/components/SidebarContainer/components/SidebarItem";
+import { DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
 
 export default function StudentSidebar() {
-  return <SidebarContainer>test</SidebarContainer>;
+  return (
+    <SidebarContainer>
+      <SidebarItem to="dashboard" icon="material-symbols:dashboard">
+        Dashboard
+      </SidebarItem>
+
+      <SidebarItem to="" icon="uil:schedule">
+        Schedule
+      </SidebarItem>
+
+      <SidebarItem to="" icon="material-symbols:analytics-outline">
+        Grades
+      </SidebarItem>
+
+      <DropdownMenuSeparator />
+
+      <SidebarItem to="" icon="mingcute:announcement-line">
+        Announcements
+      </SidebarItem>
+
+      <SidebarItem to="" icon="teenyicons:user-outline">
+        Profile
+      </SidebarItem>
+    </SidebarContainer>
+  );
 }
