cfa6ce7e68e482e4f12807d406af9470c16ed530
n0wickk <memetitv01@gmail.com>
2023-11-27 09:29:46 +0800

    Refactor sidebar item components for reusability
    Replace specific SidebarItem imports with a more generic one for
    versatility across different sidebar contexts. This enhances code
    maintainability and promotes consistent UI components.

--- src/components/layout/Sidebar/components/TeacherSidebar/index.jsx
+++ src/components/layout/Sidebar/components/TeacherSidebar/index.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import SidebarContainer from "../SidebarContainer";
+import SidebarItem from "../SidebarContainer/components/SidebarItem";
+import { DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
 
 export default function TeacherSidebar() {
-  return <SidebarContainer>teacher</SidebarContainer>;
+  return (
+    <SidebarContainer>
+      <SidebarItem to="dashboard" icon="material-symbols:dashboard">
+        Dashboard
+      </SidebarItem>
+
+      <SidebarItem to="" icon="uil:schedule">
+        Schedule
+      </SidebarItem>
+
+      <SidebarItem to="" icon="mdi:google-classroom">
+        Classes
+      </SidebarItem>
+
+      <SidebarItem to="" icon="material-symbols:analytics-outline">
+        Grades
+      </SidebarItem>
+
+      <DropdownMenuSeparator />
+
+      <SidebarItem to="" icon="mingcute:announcement-line">
+        Announcements
+      </SidebarItem>
+    </SidebarContainer>
+  );
 }
