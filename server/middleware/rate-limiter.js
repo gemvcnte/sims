90b509cafed45290266fd33600b5765fba6ff8d3
n0wickk <memetitv01@gmail.com>
2023-11-28 09:22:31 +0800

    Add track and strand on studentModel

--- server/middleware/rate-limiter.js
+++ server/middleware/rate-limiter.js
@@ -1,38 +1,34 @@
-const rateLimit = require('express-rate-limit')
+const rateLimit = require("express-rate-limit");
 const rateLimiter = rateLimit({
-    windowMs: 24 * 60 * 60 * 1000, // 24 hrs to milliseconds
-    message: "You have exceeded the 200 requests in 24 hrs limit!",
-    max: 1000,
-    standardHeaders: true,
-    legacyHeaders: false,
+  windowMs: 24 * 60 * 60 * 1000, // 24 hrs to milliseconds
+  message: "You have exceeded the 200 requests in 24 hrs limit!",
+  max: 1000,
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
-
 const rateLimiterConfig = {
-    admin:{
-        ...rateLimiter,
-        max: 500, // 500 requests limit
-        message: 'You have exceeded the 500 requests in 24hrs limit.', //message for the admin
-    },
-    teacher: {
-        ...rateLimiter,
-        max: 300, // 300 request limit
-        message: 'You have exceeded the 300 requests in 24hrs limit.' //message for the teacher
-    },
-    student:{
-        ...rateLimiter,
-        // max: 200, // 200 request limit
-        max: 10, // 200 request limit (testing)
-        message: 'You have exceeded the 200 requests in 24hrs limit.' //message for the student
-    }
-
-
-}
+  admin: {
+    ...rateLimiter,
+    max: 500, // 500 requests limit
+    message: "You have exceeded the 500 requests in 24hrs limit.", //message for the admin
+  },
+  teacher: {
+    ...rateLimiter,
+    max: 300, // 300 request limit
+    message: "You have exceeded the 300 requests in 24hrs limit.", //message for the teacher
+  },
+  student: {
+    ...rateLimiter,
+    // max: 200, // 200 request limit
+    max: 300, // 200 request limit (testing)
+    message: "You have exceeded the 200 requests in 24hrs limit.", //message for the student
+  },
+};
 
 function getRateLimiter(role) {
-    const config = rateLimiterConfig[role] || rateLimiterConfig.student
-    return rateLimit(config)
+  const config = rateLimiterConfig[role] || rateLimiterConfig.student;
+  return rateLimit(config);
 }
 
-
-module.exports = getRateLimiter 
+module.exports = getRateLimiter;
--- server/models/StudentModel.js
+++ server/models/StudentModel.js
@@ -1,128 +1,135 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // Student Schema
 const studentSchema = new mongoose.Schema(
-    {
-      firstName: {
-        type: String,
-        required: true,
-      },
-      middleName: {
-        type: String,
-        required: true,
-      },
-      lastName: {
-        type: String,
-        required: true,
-      },
-      extensionName: {
-        type: String,
-        required: true,
-      },
-      birthDate: {
-        type: String,
-        required: true,
-      },
-      gender: {
-        type: String,
-        required: true,
-      },
-      lrn: {
-        type: String,
-        required: true,
-      },
-      schoolYear: {
-        type: String,
-        required: false,
-      },
-      semester: {
-        type: String,
-      },
-      emailAddress: {
-        type: String,
-        required: true,
-        validate(value) {
-          if (!value.match(/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/)) {
-            throw new Error("Email is not valid.");
-          }
-        },
-      },
-      fatherName: {
-        type: String,
-        required: true,
-      },
-      fatherContactNumber: {
-        type: String,
-        required: true,
-      },
-      motherName: {
-        type: String,
-        required: true,
-      },
-      motherContactNumber: {
-        type: String,
-        required: true,
-      },
-      guardianName: {
-        type: String,
-        default: "none",
-      },
-      guardianContactNumber: {
-        type: String,
-        default: "none",
-      },
-      guardianRelationship: {
-        type: String,
-        default: "none",
-      },
-      lrn: {
-        type: String,
-        required: true,
-      },
-      religion: {
-        type: String,
-        default: "null",
-      },
-      province: {
-        type: String,
-        default: "null",
-      },
-      city: {
-        type: String,
-        default: "null",
-      },
-      zipCode: {
-        type: Number,
-        default: null,
-      },
-      barangay: {
-        type: String,
-        default: "null",
-      },
-      fourPs: {
-        type: Boolean,
-        default: null,
-      },
-  
-      role: {
-        type: String,
-        default: "student",
-        enum: ["student", "teacher", "admin"],
-      },
-  
-      password: {
-        type: String,
-      },
-      userImage: {
-        type: String,
-        // need nalang lagyan ng image
-        default: ''
-      },  
+  {
+    firstName: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
-  );
-  
+    middleName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    extensionName: {
+      type: String,
+      required: true,
+    },
+    birthDate: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    lrn: {
+      type: String,
+      required: true,
+    },
+    schoolYear: {
+      type: String,
+      required: false,
+    },
+    track: {
+      type: String,
+      required: false,
+    },
+    strand: {
+      type: String,
+      required: false,
+    },
+    semester: {
+      type: String,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!value.match(/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/)) {
+          throw new Error("Email is not valid.");
+        }
+      },
+    },
+    fatherName: {
+      type: String,
+      required: true,
+    },
+    fatherContactNumber: {
+      type: String,
+      required: true,
+    },
+    motherName: {
+      type: String,
+      required: true,
+    },
+    motherContactNumber: {
+      type: String,
+      required: true,
+    },
+    guardianName: {
+      type: String,
+      default: "none",
+    },
+    guardianContactNumber: {
+      type: String,
+      default: "none",
+    },
+    guardianRelationship: {
+      type: String,
+      default: "none",
+    },
+    lrn: {
+      type: String,
+      required: true,
+    },
+    religion: {
+      type: String,
+      default: "null",
+    },
+    province: {
+      type: String,
+      default: "null",
+    },
+    city: {
+      type: String,
+      default: "null",
+    },
+    zipCode: {
+      type: Number,
+      default: null,
+    },
+    barangay: {
+      type: String,
+      default: "null",
+    },
+    fourPs: {
+      type: Boolean,
+      default: null,
+    },
 
-    const Student = mongoose.model("studentEnrolled", studentSchema);
+    role: {
+      type: String,
+      default: "student",
+      enum: ["student", "teacher", "admin"],
+    },
 
-    module.exports = {Student}
+    password: {
+      type: String,
+    },
+    userImage: {
+      type: String,
+      // need nalang lagyan ng image
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const Student = mongoose.model("studentEnrolled", studentSchema);
+
+module.exports = { Student };
