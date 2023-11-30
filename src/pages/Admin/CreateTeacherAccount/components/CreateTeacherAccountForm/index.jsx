cf3c3d073dafba51cb73b0d8b4d534724493d677
n0wickk <memetitv01@gmail.com>
2023-11-30 14:44:19 +0800

    Add CreateTeacherAccountForm component to CreateTeacherAccount page
    Incorporate CreateTeacherAccountForm to streamline teacher account
    creation. Improves code organization and enhances user experience.

---
+++ src/pages/Admin/CreateTeacherAccount/components/CreateTeacherAccountForm/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CreateTeacherAccountForm() {
+  return <div>form</div>;
+}
--- src/pages/Admin/CreateTeacherAccount/index.jsx
+++ src/pages/Admin/CreateTeacherAccount/index.jsx
@@ -1,10 +1,13 @@
 import Topbar from "@/components/layout/Topbar";
 import React from "react";
+import { CreateTeacherAccountForm } from "./components";
 
 export default function CreateTeacherAccount() {
   return (
     <main className="w-full">
       <Topbar>CREATE TEACHER ACCOUNT</Topbar>
+
+      <CreateTeacherAccountForm />
     </main>
   );
 }
