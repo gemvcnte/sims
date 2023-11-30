5b8cc9fe71d641663ea3d64c6e624dff1cd97e0a
n0wickk <memetitv01@gmail.com>
2023-11-30 21:27:37 +0800

    Refactor ParentInformationSection.jsx: Remove redundant 'disabled' attributes
    Removed redundant 'disabled' attributes from InputField and select
    elements in the ParentInformationSection component to enable user input
    for guardian information, ensuring a more interactive and accessible
    user experience.

--- src/pages/Student/StudentProfile/components/StudentProfileDisplayAndEditSection/sections/ParentInformationSection.jsx
+++ src/pages/Student/StudentProfile/components/StudentProfileDisplayAndEditSection/sections/ParentInformationSection.jsx
@@ -86,7 +86,6 @@ export function ParentInformationSection({
             Guardian's Name
           </Label>
           <InputField
-            disabled
             type="text"
             placeholder="Firstname Middlename Lastname"
             value={studentProfile?.guardianName}
@@ -100,7 +99,6 @@ export function ParentInformationSection({
             Guardian's Tel. No.
           </Label>
           <InputField
-            disabled
             type="number"
             placeholder="+639xxxxxxxxx"
             value={studentProfile?.guardianContactNumber}
@@ -116,7 +114,6 @@ export function ParentInformationSection({
             Guardian Relationship
           </Label>
           <select
-            disabled
             className="col-span-3 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
             name="guardianRelationship"
             value={studentProfile?.guardianRelationship}
