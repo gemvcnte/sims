50e6f1d3c0f280801db47641ac2181897c48fa5c
n0wickk <memetitv01@gmail.com>
2023-11-30 14:37:23 +0800

    Refactor teacher account management UI and add CreateTeacherAccount page
    Updated AdminSidebar TeachersDropdown to handle different teacher
    account actions. Added CreateTeacherAccount page for admin
    functionality. Improves user experience by offering more comprehensive
    admin features without code duplication.

---
+++ src/pages/Admin/CreateTeacherAccount/index.jsx
@@ -0,0 +1,10 @@
+import Topbar from "@/components/layout/Topbar";
+import React from "react";
+
+export default function CreateTeacherAccount() {
+  return (
+    <main className="w-full">
+      <Topbar>CREATE TEACHER ACCOUNT</Topbar>
+    </main>
+  );
+}
