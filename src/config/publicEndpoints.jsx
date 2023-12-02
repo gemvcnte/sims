2ee8c0ef75e7c6dabed86352cc21f96f8af807d4
n0wickk <memetitv01@gmail.com>
2023-12-02 10:51:40 +0800

    Refactor API endpoints to centralize configuration
    Consolidates various API endpoints into centralized configuration files
    to streamline maintenance, minimize redundancy, and enhance readability.
    This change ensures easier future updates and better consistency across
    the application. No known issues or concerns from these modifications.

---
+++ src/config/publicEndpoints.jsx
@@ -0,0 +1,4 @@
+const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+
+// login
+export const loginEndpoint = `${baseUrl}/login`;
--- src/pages/Login/components/RoleSelectionContainer/LoginForm/LoginForm.jsx
+++ src/pages/Login/components/RoleSelectionContainer/LoginForm/LoginForm.jsx
@@ -4,12 +4,10 @@ import axios from "axios";
 import notify from "../../../../../utils/BlankFieldNotification";
 import { Icon } from "@iconify/react";
 import { useNavigate } from "react-router-dom";
-import { getBaseUrl } from "@src/utils/configUtils";
+import { loginEndpoint } from "@/config/publicEndpoints";
 
 function LoginForm({ role, setSelectedRole }) {
   const navigate = useNavigate();
-  const baseUrl = getBaseUrl();
-  let apiUrl = `${baseUrl}/${role}/login`;
 
   const [loginData, setLoginData] = useState({
     role: role,
@@ -25,7 +23,7 @@ function LoginForm({ role, setSelectedRole }) {
 
     if (isFormValid) {
       try {
-        const response = await axios.post(apiUrl, loginData);
+        const response = await axios.post(loginEndpoint, loginData);
         console.log(apiUrl);
 
         if (response.status === 200) {
@@ -79,7 +77,7 @@ function LoginForm({ role, setSelectedRole }) {
             onChange={(e) => handleInputChange("username", e.target.value)}
             required
             placeholder={`${role} Username`}
-            className="w-full rounded-md border-b border-white-700 p-3 placeholder-white-700 focus:border focus:border-blue-400 focus:outline-none"
+            className="border-white-700 placeholder-white-700 w-full rounded-md border-b p-3 focus:border focus:border-blue-400 focus:outline-none"
           />
         </div>
 
@@ -106,12 +104,12 @@ function LoginForm({ role, setSelectedRole }) {
             onChange={(e) => handleInputChange("password", e.target.value)}
             required
             placeholder={`${role} Password`}
-            className="w-full rounded-md border-b border-white-700 p-3 placeholder-white-700 focus:border focus:border-blue-400 focus:outline-none"
+            className="border-white-700 placeholder-white-700 w-full rounded-md border-b p-3 focus:border focus:border-blue-400 focus:outline-none"
           />
         </div>
 
         <button
-          className="mt-4 flex items-center justify-center gap-2 rounded-2xl bg-blue-400 px-10 py-3 text-white-400 transition-all duration-300 hover:gap-8"
+          className="text-white-400 mt-4 flex items-center justify-center gap-2 rounded-2xl bg-blue-400 px-10 py-3 transition-all duration-300 hover:gap-8"
           type="submit"
         >
           Login
