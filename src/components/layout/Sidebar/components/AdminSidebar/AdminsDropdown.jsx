a4e1146e47230ec78d07dc6087cf7bfc8b17bc9d
n0wickk <memetitv01@gmail.com>
2023-12-02 20:03:56 +0800

    Add AdminsDropdown component to AdminSidebar
    Add the AdminsDropdown component to the AdminSidebar to enable
    administrators to manage admin-related tasks easily. This enhancement
    provides a more comprehensive administrative interface.

---
+++ src/components/layout/Sidebar/components/AdminSidebar/AdminsDropdown.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useSidebarContext } from "@/contexts/SidebarContext.jsx";
+
+export function AdminsDropdown({}) {
+  const { toggleSidebar } = useSidebarContext();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleDropdownClick = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+    toggleSidebar();
+  };
+
+  return (
+    <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+      <DropdownMenuTrigger>
+        <Button variant="ghost" className="w-full justify-between ">
+          <span className="flex items-center">
+            <Icon icon="mdi:teacher" className="mr-2" />
+            Admins
+          </span>
+          <span>
+            {!isDropdownOpen ? (
+              <Icon icon="gridicons:dropdown" className="mr-2" />
+            ) : (
+              <Icon icon="gridicons:dropdown" className="mr-2" rotate={90} />
+            )}
+          </span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start">
+        <DropdownMenuLabel>Admins</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link onClick={handleDropdownClick}>Update Admin Password</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem>
+          <Link to="" onClick={handleDropdownClick}>
+            Create Admin Account
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
