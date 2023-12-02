282e291e32077e7092b646dbc0bd5f357e6e6779
ampol <memetitv01@gmail.com>
2023-12-02 16:45:59 +0800

    Update/server (#33)
    * Add currentAddress field in StudentModel
    
    * remove getStudentDashboard
    
    * Update adminController.js
    
    * test
    
    * concactenate firstName and lastName to fullName in the tokenPayload
    
    * fixed createTeacher
    
    * change the status code of CREATE from 200 to 201
    
    * none change
    
    * added getTeacherProfile
    
    * removed irrelevant code in the getStudentProfile
    
    * added a route for the teacher profile  with a method of get
    
    * test
    
    * added new fields in the teacherModel 😙 boss!
    
    * optimize studentApplicationController for improved performance
    
    * changed something
    
    * Update createTeacher function in adminController
    
    ---------
    
    Co-authored-by: gem <111511069+gemvcnte@users.noreply.github.com>
    Co-authored-by: gemvcnte <gemvicente6@gmail.com>

--- server/models/TeacherModel.js
+++ server/models/TeacherModel.js
@@ -1,108 +1,106 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 // Teacher Schema
 const teacherSchema = new mongoose.Schema(
-    {
-      firstName: {
-        type: String,
-        required: true,
-        default: "",
-        uppercase: true,
+  {
+    firstName: {
+      type: String,
+      required: true,
+      default: "",
+      uppercase: true,
+    },
+    middleName: {
+      type: String,
+      // required: true,
+      default: "",
+      uppercase: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      default: "",
+      uppercase: true,
+    },
+    currentAddress: {
+      type: String,
+      // required: true,
+      default: "",
+      uppercase: true,
+    },
+    birthDate: {
+      type: String,
+      required: true,
+      default: "",
+      uppercase: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      default: "",
+      uppercase: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!value.match(/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/)) {
+          throw new Error("Email is not valid.");
+        }
       },
-      middleName: {
-        type: String,
-        required: true,
-        default: "",
-        uppercase: true,
-      },
-      lastName: {
-        type: String,
-        required: true,
-        default: "",
-        uppercase: true,
-      },
-      currentAddress: {
-        type: String,
-        required: true,
-        default: "",
-        uppercase: true,
-      },
-      birthDate: {
-        type: String,
-        required: true,
-        default: "",
-        uppercase: true,
-      },
-      gender: {
-        type: String,
-        required: true,
-        default: "",
-        uppercase: true,
-      },
-      username: {
-        type: String,
-        required: true,
-        default: "",
-      },
-      emailAddress: {
-        type: String,
-        required: true,
-        validate(value) {
-          if (!value.match(/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/)) {
-            throw new Error("Email is not valid.");
-          }
-        },
-        default: "",
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-  
-      role: {
-        type: String,
-        default: "teacher",
-        enum: ["student", "teacher", "admin"],
-      },
-      userImage: {
-        type: String,
-        // need nalang lagyan ng image
-        default: ''
-      },
-      // designation: {
-      //   type: String,
-      //   required: true,
-      // },
-      // numOfYearsTeaching: {
-      //   type: Number,
-      //   required: true,
-      // },
-      // specialization: {
-      //   type: String,
-      //   required: true,
-      // },
-      // highestEducationalAttainment: {
-      //   type: String,
-      //   required: true,
-      // },
-      // tinNumber: {
-      //   type: Number,
-      //   required: true,
-      // },
-      // gsisNumber: {
-      //   type: Number,
-      //   required: true,
-      // },
-      // plantillaNumber: {
-      //   type: Number,
-      //   required: true,
-      // },
+      default: "",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
 
-},
-    { timestamps: true }
-  );
+    role: {
+      type: String,
+      default: "teacher",
+      enum: ["student", "teacher", "admin"],
+    },
+    userImage: {
+      type: String,
+      // need nalang lagyan ng image
+      default: "",
+    },
+    // designation: {
+    //   type: String,
+    //   required: true,
+    // },
+    // numOfYearsTeaching: {
+    //   type: Number,
+    //   required: true,
+    // },
+    // specialization: {
+    //   type: String,
+    //   required: true,
+    // },
+    // highestEducationalAttainment: {
+    //   type: String,
+    //   required: true,
+    // },
+    // tinNumber: {
+    //   type: Number,
+    //   required: true,
+    // },
+    // gsisNumber: {
+    //   type: Number,
+    //   required: true,
+    // },
+    // plantillaNumber: {
+    //   type: Number,
+    //   required: true,
+    // },
+  },
+  { timestamps: true }
+);
 
-const Teacher = mongoose.model("Teacher", teacherSchema)
+const Teacher = mongoose.model("Teacher", teacherSchema);
 
-module.exports = {Teacher}
+module.exports = { Teacher };
