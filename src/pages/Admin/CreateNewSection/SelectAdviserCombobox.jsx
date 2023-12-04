b4b95b27f9a551b840fc13c21a82283045c157fb
n0wickk <memetitv01@gmail.com>
2023-12-04 15:29:53 +0800

    Refactor package dependencies and update UI dialog
    Added "@radix-ui/react-popover" and "cmdk" dependencies to
    package-lock.json and package.json. Updated UI dialog title and button
    labels for clarity and consistency. Reorganized dialog form fields for
    creating a new section in the admin panel. This improves user experience
    and maintainability.

---
+++ src/pages/Admin/CreateNewSection/SelectAdviserCombobox.jsx
@@ -0,0 +1,87 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+const frameworks = [
+  {
+    value: "gem vicente",
+    label: "Gem Vicente",
+  },
+  {
+    value: "railey murillo",
+    label: "Railey Murillo",
+  },
+  {
+    value: "nathaniel silvino",
+    label: "Nathaniel Silvino",
+  },
+  {
+    value: "john paulo gabornes",
+    label: "John Paulo Gabornes",
+  },
+];
+
+export default function SelectAdviserCombobox() {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="col-span-3 flex h-10  w-full justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+        >
+          {value
+            ? frameworks.find((framework) => framework.value === value)?.label
+            : "Select teacher..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search teacher..." />
+          <CommandEmpty>No framework found.</CommandEmpty>
+          <CommandGroup>
+            {frameworks.map((framework) => (
+              <CommandItem
+                key={framework.value}
+                value={framework.value}
+                onSelect={(currentValue) => {
+                  setValue(currentValue === value ? "" : currentValue);
+                  setOpen(false);
+                }}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    value === framework.value ? "opacity-100" : "opacity-0",
+                  )}
+                />
+                {framework.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
--- src/pages/Admin/CreateNewSection/index.jsx
+++ src/pages/Admin/CreateNewSection/index.jsx
@@ -10,12 +10,13 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import SelectAdviserCombobox from "./SelectAdviserCombobox";
 
 export default function CreateNewSection({ onClose }) {
   return (
     <DialogContent className="sm:max-w-[425px]">
       <DialogHeader>
-        <DialogTitle>Edit profile</DialogTitle>
+        <DialogTitle>Create a New Section</DialogTitle>
         <DialogDescription>
           Make changes to your profile here. Click save when you're done.
         </DialogDescription>
@@ -25,21 +26,22 @@ export default function CreateNewSection({ onClose }) {
           <Label htmlFor="name" className="text-right">
             Name
           </Label>
-          <Input id="name" defaultValue="Pedro Duarte" className="col-span-3" />
-        </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="username" className="text-right">
-            Username
-          </Label>
           <Input
-            id="username"
-            defaultValue="@peduarte"
+            id="name"
+            placeholder="Enter Section Name"
+            defaultValue=""
             className="col-span-3"
           />
         </div>
+        <div className="grid grid-cols-4 items-center gap-4">
+          <Label htmlFor="username" className="text-right">
+            Adviser
+          </Label>
+          <SelectAdviserCombobox />
+        </div>
       </div>
       <DialogFooter>
-        <Button onClick={onClose}>Save changes</Button>
+        <Button onClick={onClose}>Create Section</Button>
       </DialogFooter>
     </DialogContent>
   );
