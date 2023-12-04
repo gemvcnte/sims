90d6c4a52ae7454bf6f5cd66b7f258c7106d4264
gemvcnte <gemvicente6@gmail.com>
2023-12-04 20:19:17 +0800

    refactor ClassroomModel.js for improved clarity

--- server/models/ClassroomModel.js
+++ server/models/ClassroomModel.js
@@ -1,35 +1,37 @@
+const { Teacher } = require('../models/TeacherModel')
 const mongoose = require('mongoose');
 
-
-const classroomSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    section: {
-        type: String,
-        required: true,
-    },
-    adviser: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Teacher',
-        required: true,
-    },
-    students: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
+const classroomSchema = mongoose.Schema(
+    {
+        section: {
+            type: String,
             required: true,
         },
-    ],
-    announcements: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
+        gradeLevel: {
+            type: Number,
+            required: true,
+        },
+        adviser: {
+            type: String,
+            required: true,
+        },
+        subjects: [
+            {
+                subjectName: {
+                    type: String,
+                    required: true,
+                },
+                teacher: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'Teacher',
+                    required: true,
+                },
+            },
+        ],
     },
-}, {timestamps: true})
+    { timestamps: true }
+);
 
+const Classroom = mongoose.model('Classroom', classroomSchema);
 
-const Classroom = mongoose.model('Classroom', classroomSchema)
-
-module.exports = { Classroom }
-
-
+module.exports = { Classroom };
