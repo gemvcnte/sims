51957718d2cefebfa52ee86b7702589003569b70
gemvcnte <gemvicente6@gmail.com>
2023-12-06 01:44:00 +0800

    test/proto schema(not yet final)

--- server/models/ScheduleModel.js
+++ server/models/ScheduleModel.js
@@ -2,9 +2,25 @@ const mongoose = require('mongoose')
 
 
 const scheduleSchema = mongoose.Schema({
-    
+    subjectTitle: {
+        type: String,
+        required: true,
+    },
+    days: {
+        type: String,
+        enum: ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+        required: true,
+    },
+    startTime: {
+        type: String,
+        required: true,
+    },
+    endTime: {
+        type: String,
+        required: true,
+    },
 })
 
-const Schedule = mongoose.model('schedule', )
+const Schedule = mongoose.model('schedule', scheduleSchema )
 
 module.exports = {Schedule}
