5c9862fdaf7292f1e5c117041630ba4eb12b8b08
n0wickk <memetitv01@gmail.com>
2023-12-06 19:41:08 +0800

    Refactor topbar component to include section analytics
    Integrate SectionAnalytics component within Topbar to provide analytics
    for sections. This enhances user experience by offering valuable
    insights.

---
+++ src/pages/Admin/Sections/components/index.jsx
@@ -0,0 +1,1 @@
+export { default as SectionAnalytics } from "./SectionAnalytics";
--- src/pages/Admin/Sections/index.jsx
+++ src/pages/Admin/Sections/index.jsx
@@ -1,10 +1,13 @@
 import Topbar from "@/components/layout/Topbar";
 import React from "react";
+import { SectionAnalytics } from "./components";
 
 export default function Sections() {
   return (
     <main className="w-full">
       <Topbar>SECTIONS</Topbar>
+
+      <SectionAnalytics />
     </main>
   );
 }
