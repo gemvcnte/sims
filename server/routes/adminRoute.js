6d7530d6bd369d98f7ff7f489a50914e7b638db2
n0wickk <memetitv01@gmail.com>
2023-12-09 13:01:14 +0800

    Bug(login): remove verifyToken on /login routes

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -6,11 +6,9 @@ const verifyToken = require("../middleware/verifyToken");
 dotenv.config();
 
 // Admin routes for admin...
-router.post("/create", 
-  verifyToken, 
-  adminController.createAdmin);
-router.post("/login", verifyToken, adminController.adminLogin);
-router.patch("/updateAdmin",  verifyToken, adminController.updateAdmin);
+router.post("/login", adminController.adminLogin);
+router.post("/create", verifyToken, adminController.createAdmin);
+router.patch("/updateAdmin", verifyToken, adminController.updateAdmin);
 router.delete("/deleteAdmin", verifyToken, adminController.deleteAdmin);
 
 // Teacher routes for admin...
@@ -27,16 +25,40 @@ router.get("/getAllStudents", verifyToken, adminController.getAllStudents);
 router.get("/getPending", verifyToken, adminController.getAllPending); // /pending
 router.get("/getApproved", verifyToken, adminController.getAllApproved); // /approved
 router.get("/getRejected", verifyToken, adminController.getAllRejected); // /rejected
-router.get("/getSpecificStudent", verifyToken, adminController.getSpecificStudent); // /getID/:id
+router.get(
+  "/getSpecificStudent",
+  verifyToken,
+  adminController.getSpecificStudent
+); // /getID/:id
 
 // StudentApplication routes for admin
-router.post("/enrollStudent", verifyToken, adminController.acceptStudentApplication);
-router.patch("/updateApplication", verifyToken, adminController.updateStudentApplication);
-router.patch("/rejectApplication", verifyToken, adminController.rejectStudentApplication);
+router.post(
+  "/enrollStudent",
+  verifyToken,
+  adminController.acceptStudentApplication
+);
+router.patch(
+  "/updateApplication",
+  verifyToken,
+  adminController.updateStudentApplication
+);
+router.patch(
+  "/rejectApplication",
+  verifyToken,
+  adminController.rejectStudentApplication
+);
 
-router.post("/createAnnouncement", verifyToken, adminController.createSchoolAnnouncement);
+router.post(
+  "/createAnnouncement",
+  verifyToken,
+  adminController.createSchoolAnnouncement
+);
 router.patch("/updateAnnouncement", adminController.updateSchoolAnnouncement);
-router.delete("/deleteAnnouncement", verifyToken, adminController.deleteSchoolAnnouncement);
+router.delete(
+  "/deleteAnnouncement",
+  verifyToken,
+  adminController.deleteSchoolAnnouncement
+);
 
 // Admin Profile
 router.get("/profile", verifyToken, adminController.getAdminProfile);
@@ -47,10 +69,10 @@ router.patch(
 );
 
 // Class functions
-router.post("/class/create", verifyToken, adminController.createClassroom) // /alt. /class/create-classroom
-router.get("/class/getAll", verifyToken, adminController.getAllClasses)
-router.get("/class/:id", verifyToken,  adminController.getSpecificClass)
-router.patch("/class/update", verifyToken, adminController.getSpecificClass)
-router.delete("/class/delete", verifyToken, adminController.deleteClassroom)
+router.post("/class/create", verifyToken, adminController.createClassroom); // /alt. /class/create-classroom
+router.get("/class/getAll", verifyToken, adminController.getAllClasses);
+router.get("/class/:id", verifyToken, adminController.getSpecificClass);
+router.patch("/class/update", verifyToken, adminController.getSpecificClass);
+router.delete("/class/delete", verifyToken, adminController.deleteClassroom);
 
 module.exports = router;
--- server/routes/teacherRoute.js
+++ server/routes/teacherRoute.js
@@ -6,17 +6,27 @@ const verifyToken = require("../middleware/verifyToken");
 dotenv.config;
 
 // router.post('/login', expressjwt({secret: process.env.JWT_SECRET}), teacherController.teacherLogin)
-router.post("/login", verifyToken,  teacherController.teacherLogin);
+router.post("/login", teacherController.teacherLogin);
 router.get("/profile", verifyToken, teacherController.getTeacherProfile);
-router.post("/announcement/classPost", verifyToken, teacherController.postClassAnnouncement);
+router.post(
+  "/announcement/classPost",
+  verifyToken,
+  teacherController.postClassAnnouncement
+);
 router.patch(
   "/profile/update",
   verifyToken,
   teacherController.updateTeacherProfile
 );
-router.patch("/class/assignStudent", verifyToken, teacherController.assignStudentToClass)
-router.delete("/class/remove-student", verifyToken, teacherController.removeStudentToClass)
-
-
+router.patch(
+  "/class/assignStudent",
+  verifyToken,
+  teacherController.assignStudentToClass
+);
+router.delete(
+  "/class/remove-student",
+  verifyToken,
+  teacherController.removeStudentToClass
+);
 
 module.exports = router;
