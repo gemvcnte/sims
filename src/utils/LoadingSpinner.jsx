e4c895677a3160dc5eb9354c9f58677f467507e2
ampol <memetitv01@gmail.com>
2023-12-13 21:12:16 +0800

    Fix LoadingSpinner overlay color (#70)
    improved visibility
    
    Adjust the overlay opacity to enhance spinner visibility, ensuring a
    better user experience when loading content. Fixes #55

--- src/utils/LoadingSpinner.jsx
+++ src/utils/LoadingSpinner.jsx
@@ -4,7 +4,7 @@ import { SyncLoader } from "react-spinners";
 const LoadingSpinner = () => {
   return (
     <>
-      <div className="absolute z-10 h-[100vh] w-[100vw] bg-black-400 opacity-60"></div>
+      <div className="absolute z-10 h-[100vh] w-[100vw] bg-black opacity-80"></div>
       <div className="absolute z-20  flex h-[100vh] w-[100vw] items-center justify-center">
         <SyncLoader color={"#18b7f7"} loading={true} size={15} />
       </div>
