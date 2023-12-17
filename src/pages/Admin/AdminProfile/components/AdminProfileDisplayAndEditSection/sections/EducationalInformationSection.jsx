913710aebd922ec1b561a7293714517348c46ddf
ampol <memetitv01@gmail.com>
2023-12-17 15:52:21 +0800

    Refactor labels for educational information input (#71)
    fields
    
    Simplify and clarify labels for teaching years and educational
    attainment fields across admin and teacher profile sections. This
    improves user understanding and overall user experience.

--- src/pages/Admin/AdminProfile/components/AdminProfileDisplayAndEditSection/sections/EducationalInformationSection.jsx
+++ src/pages/Admin/AdminProfile/components/AdminProfileDisplayAndEditSection/sections/EducationalInformationSection.jsx
@@ -19,7 +19,7 @@ export function EducationalInformationSection({
       <main className="flex w-full flex-col gap-4">
         <div className="grid grid-cols-4 items-center gap-4">
           <Label htmlFor="numOfYearsTeaching" className="text-right">
-            Number of Years Teaching
+            Teaching Year/s
           </Label>
           <InputField
             type="number"
@@ -61,7 +61,7 @@ export function EducationalInformationSection({
 
         <div className="grid grid-cols-4 items-center gap-4">
           <Label htmlFor="specialization" className="text-right">
-            Area of Specialization
+            Specialization
           </Label>
           <select
             className="col-span-3 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
@@ -79,7 +79,8 @@ export function EducationalInformationSection({
 
         <div className="grid grid-cols-4 items-center gap-4">
           <Label htmlFor="highestEducationalAttainment" className="text-right">
-            Highest Educational Attainment
+            Highest <span className="hidden sm:block">Educational</span>{" "}
+            Attainment
           </Label>
           <select
             className="col-span-3 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
