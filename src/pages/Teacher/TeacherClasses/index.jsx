583c9da7b038e56ce7c849e3351636c563186c1d
n0wickk <memetitv01@gmail.com>
2023-12-20 22:40:27 +0800

    Add TeacherClassesSection

--- src/pages/Teacher/TeacherClasses/index.jsx
+++ src/pages/Teacher/TeacherClasses/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Topbar from "@/components/layout/Topbar";
+import TeacherClassesSection from "./partials/TeacherClassesSection";
 
 const TeacherClasses = () => {
   return (
     <main className="w-full">
       <Topbar>CLASSES</Topbar>
+
+      <TeacherClassesSection />
     </main>
   );
 };
---
+++ src/pages/Teacher/TeacherClasses/partials/TeacherClassesSection.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TeacherClassesSection() {
+  return <section>teacher classes section</section>;
+}
