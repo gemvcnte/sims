1930d1414e8e71147c181bfa376d1e6767685011
n0wickk <memetitv01@gmail.com>
2023-12-20 22:34:03 +0800

    Update(teacherEndpoints): Add getTeacherAssignedClasses endpoint

--- src/config/teacherEndpoints.jsx
+++ src/config/teacherEndpoints.jsx
@@ -1,4 +1,8 @@
 const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
+// profile
 export const getTeacherProfileEndpoint = `${baseUrl}/teacher/profile`;
 export const updateTeacherProfileEndpoint = `${baseUrl}/teacher/profile/update`;
+
+// class
+export const getTeacherAssignedClasses = `${baseUrl}/teacher/class/getAssignedClasses`;
