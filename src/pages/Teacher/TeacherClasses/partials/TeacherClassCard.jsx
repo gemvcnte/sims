0db8a36c8c7780b0e7bce5563e31d04b81c0e499
n0wickk <memetitv01@gmail.com>
2023-12-20 23:53:20 +0800

    Refactor TeacherClassCard component styles
    Enhanced styling for TeacherClassCard component to improve visual
    consistency and readability. Reconfigured border and background
    properties. No known issues.

--- src/pages/Teacher/TeacherClasses/partials/TeacherClassCard.jsx
+++ src/pages/Teacher/TeacherClasses/partials/TeacherClassCard.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function TeacherClassCard({ sectionName }) {
   return (
-    <section className="flex w-full items-center justify-between rounded-lg border border-foreground bg-card p-4 text-card-foreground shadow-sm">
+    <section className="flex w-full items-center justify-between rounded-lg border bg-card p-4 text-card-foreground shadow-sm">
       <span className="font-bold uppercase">{sectionName}</span>
       <Button variant="ghost">Edit</Button>
     </section>
