5a6ad691888d5cbae3cfb446a1c2f478a3e36317
n0wickk <memetitv01@gmail.com>
2023-12-24 11:20:34 +0800

    Refactor StudentsTable to dynamically fetch and update students
    Introduce state management for fetching all students, editing state, and
    handling student selection. Enable saving updated student list to the
    server. Improve user interaction by adding edit and save buttons.

--- src/pages/Teacher/TeacherSpecificClass/partials/StudentsTable.jsx
+++ src/pages/Teacher/TeacherSpecificClass/partials/StudentsTable.jsx
@@ -1,37 +1,98 @@
-// StudentsTable.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableFooter,
-  TableHead,
-  TableHeader,
   TableRow,
 } from "@/components/ui/table";
 import { Icon } from "@iconify/react";
+import axios from "axios";
 
 export default function StudentsTable({ classDetails }) {
+  const [allStudents, setAllStudents] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [selectedStudents, setSelectedStudents] = useState([]);
+
+  useEffect(() => {
+    const fetchAllStudents = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/teacher/getStudents",
+        );
+        setAllStudents(response.data.data);
+      } catch (error) {
+        console.error("Error fetching all students:", error);
+      }
+    };
+
+    if (isEditing) {
+      fetchAllStudents();
+      setSelectedStudents(
+        classDetails.students.map((student) => student.emailAddress),
+      );
+    } else {
+      setAllStudents(classDetails.students);
+      setSelectedStudents(classDetails.students.map((student) => student._id));
+    }
+  }, [isEditing, classDetails]);
+
+  const handleAddStudent = (studentEmail) => {
+    const updatedSelectedStudents = [...selectedStudents];
+
+    if (updatedSelectedStudents.includes(studentEmail)) {
+      updatedSelectedStudents.splice(
+        updatedSelectedStudents.indexOf(studentEmail),
+        1,
+      );
+    } else {
+      updatedSelectedStudents.push(studentEmail);
+    }
+
+    console.log(updatedSelectedStudents);
+    setSelectedStudents(updatedSelectedStudents);
+  };
+
+  const handleSaveChanges = async () => {
+    try {
+      await axios.post(
+        "http://localhost:5000/teacher/update-students-in-class",
+        {
+          classId: classDetails._id,
+          studentEmails: selectedStudents,
+        },
+      );
+
+      setSelectedStudents([]);
+      setIsEditing(false);
+    } catch (error) {
+      console.error("Error updating students in class:", error);
+    }
+  };
+
   return (
     <main className="p-4">
+      <button onClick={() => setIsEditing((prev) => !prev)}>
+        {isEditing ? "Cancel Editing" : "Edit Students"}
+      </button>
+      {isEditing && <button onClick={handleSaveChanges}>Save Changes</button>}
+
       <Table>
-        <TableCaption>
-          {classDetails.students.length == 0 && "No Students Found"}
-        </TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Lastname</TableHead>
-            <TableHead>Firstname</TableHead>
-            <TableHead></TableHead>
-          </TableRow>
-        </TableHeader>
         <TableBody>
-          {classDetails.students.map((student) => (
+          {allStudents.map((student) => (
             <TableRow
               key={student._id}
               className="group transition-all duration-700 hover:cursor-pointer"
             >
+              {isEditing && (
+                <TableCell>
+                  <input
+                    type="checkbox"
+                    checked={selectedStudents.includes(student.emailAddress)}
+                    onChange={() => handleAddStudent(student.emailAddress)}
+                  />
+                </TableCell>
+              )}
               <TableCell className="uppercase">{student.lastName}</TableCell>
               <TableCell>{student.firstName}</TableCell>
               <TableCell className="inline-block">
