ffe455ce6fba1527b957d2457c96e16aae3fd387
n0wickk <memetitv01@gmail.com>
2023-12-25 15:41:39 +0800

    Add AnnouncementsModal for fetching and displaying announcements
    Introduce AnnouncementsModal component to fetch announcements from the
    API endpoint and display them in the Login page. This adds valuable
    information for users and enhances the user experience.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import RoleSelectionContainer from "./components/RoleSelectionContainer";
 import Header from "./components/Header";
+import AnnouncementsModal from "./components/AnnouncementsModal";
 
 function Login() {
   useEffect(() => {
@@ -11,8 +12,27 @@ function Login() {
     };
   }, []);
 
+  const [announcements, setAnnouncements] = useState([]);
+
+  // Fetch announcements from your API endpoint
+  useEffect(() => {
+    const fetchAnnouncements = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/announcement");
+        const data = await response.json();
+        setAnnouncements(data.data); // Assuming data is structured as in your server response
+      } catch (error) {
+        console.error("Error fetching announcements:", error);
+      }
+    };
+
+    fetchAnnouncements();
+  }, []);
+
   return (
     <>
+      <AnnouncementsModal announcements={announcements} />
+
       <Header />
       <RoleSelectionContainer />
     </>
---
+++ src/pages/Login/components/AnnouncementsModal/index.jsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Icon } from "@iconify/react";
+
+export default function AnnouncementModal({ announcements }) {
+  const [isOpen, setIsOpen] = useState(true);
+  const [expandedAnnouncement, setExpandedAnnouncement] = useState(null);
+
+  const toggleContent = (announcementId) => {
+    setExpandedAnnouncement((prevExpanded) =>
+      prevExpanded === announcementId ? null : announcementId,
+    );
+  };
+
+  const sortedAnnouncements = [...announcements].sort(
+    (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+  );
+
+  return (
+    <Dialog
+      className=""
+      isOpen={isOpen}
+      defaultOpen={true}
+      onOpenChange={setIsOpen}
+    >
+      <DialogContent className="sm:max-w-[600px]">
+        <DialogHeader>
+          <DialogTitle>Announcements</DialogTitle>
+          <DialogDescription>
+            Here are the latest announcements.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="max-h-80 overflow-y-auto">
+          {sortedAnnouncements.map((announcement) => (
+            <div key={announcement._id} className="mb-4 rounded-lg border p-4">
+              <div className="flex items-center justify-between">
+                <div>
+                  <h2 className="break-words font-semibold">
+                    {announcement.title}
+                  </h2>
+                  <p className="text-xs text-muted-foreground">
+                    {new Date(announcement.createdAt).toLocaleDateString(
+                      "en-US",
+                      {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      },
+                    )}
+                  </p>
+                </div>
+              </div>
+              <button
+                className="mt-4 flex justify-center gap-1 text-xs"
+                variant="outline"
+                onClick={() => toggleContent(announcement._id)}
+              >
+                {expandedAnnouncement === announcement._id ? (
+                  <>
+                    Hide Details
+                    <Icon icon="dashicons:arrow-up" rotate={2} />
+                  </>
+                ) : (
+                  <>
+                    Show Details
+                    <Icon icon="dashicons:arrow-up" rotate={1} />
+                  </>
+                )}
+              </button>
+              {expandedAnnouncement === announcement._id && (
+                <div className="mt-2 text-sm text-gray-400">
+                  {announcement.content}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
