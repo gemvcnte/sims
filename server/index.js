d546356296e31b31eec031f1d1386364519c3946
n0wickk <memetitv01@gmail.com>
2023-12-25 16:07:06 +0800

    Update(index.js): Add publicAnnouncementRoute
    Add /announcement endpoint that returns the public announcements made by the admins

--- server/index.js
+++ server/index.js
@@ -4,69 +4,67 @@
 // import cors from 'cors';
 // import dotenv from 'dotenv';
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const connectDb = require('./db/database')
-const dbConn = require('./db/dbConnection')
-const helmet = require('helmet')
-const morgan = require('morgan')
-const getRateLimiter = require('./middleware/rate-limiter')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDb = require("./db/database");
+const dbConn = require("./db/dbConnection");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const getRateLimiter = require("./middleware/rate-limiter");
 
-
-
-connectDb()
+connectDb();
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 // app.use(rateLimiter)
 
 const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
-}
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
-app.use(express.json())
-
+app.use(express.json());
 
 // Define your routes and API endpoints here
 // app.get('/', (req, res) => {
 //     res.send('Hello, this is your Express server!');
 // });
 
-const adminRateLimiter = getRateLimiter('admin')
-const teacherRateLimiter = getRateLimiter('teacher')
-const studentRateLimiter = getRateLimiter('student')
-
-
+const adminRateLimiter = getRateLimiter("admin");
+const teacherRateLimiter = getRateLimiter("teacher");
+const studentRateLimiter = getRateLimiter("student");
 
 // Apply @ school
-const studentApplicationRoute = require('./routes/studentApplicationRoute');
-app.use('/apply', studentApplicationRoute);
+const studentApplicationRoute = require("./routes/studentApplicationRoute");
+app.use("/apply", studentApplicationRoute);
+
+// Announcements for Login Page
+const publicAnnouncementRoute = require("./routes/publicAnnouncementRoute");
+app.use("/announcement", publicAnnouncementRoute);
 
 // Admin routes
-const adminRoute = require('./routes/adminRoute');
-app.use('/admin',  adminRateLimiter, adminRoute);
+const adminRoute = require("./routes/adminRoute");
+app.use("/admin", adminRateLimiter, adminRoute);
 
 // Student routes
-const studentRoute = require('./routes/studentRoute');
-app.use('/student', studentRateLimiter, studentRoute);
+const studentRoute = require("./routes/studentRoute");
+app.use("/student", studentRateLimiter, studentRoute);
 
 // Teacher routes
-const teacherRoute = require('./routes/teacherRoute');
-app.use('/teacher', teacherRateLimiter, teacherRoute);
+const teacherRoute = require("./routes/teacherRoute");
+app.use("/teacher", teacherRateLimiter, teacherRoute);
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
