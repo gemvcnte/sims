1655bdfb37b6e73c7b5b0fe82cad5a335fe17a31
n0wickk <memetitv01@gmail.com>
2023-12-26 13:26:06 +0800

    Add SubjectsTable to TableController for displaying subjects. Improve modularity and segregation of concerns for better organization and maintainability.

---
+++ src/pages/Teacher/TeacherSpecificClass/partials/SubjectsTable.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { useClassDetails } from "../contexts/ClassDetailsContext";
+import { Icon } from "@iconify/react";
+
+export default function SubjectsTable() {
+  const classDetailsContext = useClassDetails();
+  const { classDetails, loading, fetchClassDetails } = classDetailsContext;
+
+  console.log(classDetails);
+
+  const [formData, setFormData] = useState({
+    classId: "yourClassId",
+    subjectName: "",
+    subjectTeacher: "",
+    day: "",
+    startTime: "",
+    endTime: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://your-api-base-url/api/class/add-subject",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        },
+      );
+
+      if (!response.ok) {
+        // Handle error
+        console.error("Error:", response.statusText);
+      }
+
+      const result = await response.json();
+      console.log(result.message);
+    } catch (error) {
+      // Handle error
+      console.error("Error:", error.message);
+    }
+  };
+
+  return (
+    <main className="p-4">
+      <div className="mb-4 flex gap-4">
+        <Button variant="outline">Add Subject</Button>
+      </div>
+
+      <Table>
+        {/* <TableCaption className="pb-4">No Subjects Found</TableCaption> */}
+        <TableHeader>
+          <TableRow>
+            <TableHead>Subject Name</TableHead>
+            <TableHead>Teacher</TableHead>
+            <TableHead></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {classDetails.subjects.map((subject) => (
+            <TableRow key={subject._id}>
+              <TableCell className="font-medium">
+                {subject.subjectName}
+              </TableCell>
+              <TableCell>{subject.subjectTeacher}</TableCell>
+              <TableCell className="inline-block">
+                Update <span className="hidden sm:inline">Subject</span>
+                <Icon
+                  icon="octicon:arrow-down-24"
+                  rotate={3}
+                  className="ml-2 inline-block -rotate-45 transform transition-all group-hover:rotate-45"
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter></TableFooter>
+      </Table>
+    </main>
+  );
+}
--- src/pages/Teacher/TeacherSpecificClass/partials/TableController.jsx
+++ src/pages/Teacher/TeacherSpecificClass/partials/TableController.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { useClassNav } from "../contexts/ClassNavContext";
 import StudentsTable from "./StudentsTable";
+import SubjectsTable from "./SubjectsTable";
 
 export default function TableController() {
   const { selectedTab, setTab } = useClassNav();
 
   if (selectedTab === "students") {
     return <StudentsTable />;
+  }
+
+  if (selectedTab === "subjects") {
+    return <SubjectsTable />;
   }
 
   return <div className="p-4">table controller</div>;
