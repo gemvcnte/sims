a1cbc4703d1e60b944e5149e85222b018f7354fd
n0wickk <memetitv01@gmail.com>
2023-12-26 22:27:25 +0800

    Refactor ClassNav to Conditionally Render Subjects Tab
    Add conditional rendering for the "Subjects" tab based on the user's
    role. This change allows displaying the tab only for class advisers.
    Improved user experience and interface clarity.

--- src/pages/Teacher/TeacherSpecificClass/partials/ClassNav.jsx
+++ src/pages/Teacher/TeacherSpecificClass/partials/ClassNav.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import { useClassNav } from "../contexts/ClassNavContext";
+import { useClassDetails } from "../contexts/ClassDetailsContext";
+import { isClassAdviser } from "../helpers/isClassAdviser";
 
 function ClassNav({ className, ...props }) {
+  const classDetailsContext = useClassDetails();
+  const { classDetails, loading, fetchClassDetails } = classDetailsContext;
+  const isAdviser = isClassAdviser(classDetails);
+
   const { selectedTab, setTab } = useClassNav();
 
   console.log(selectedTab);
@@ -25,15 +31,19 @@ function ClassNav({ className, ...props }) {
       >
         Students
       </p>
-      <p
-        className={`py-4 text-sm font-medium text-muted-foreground transition-colors hover:cursor-pointer hover:text-foreground ${
-          selectedTab === "subjects" &&
-          "border-b-2 border-foreground !text-foreground"
-        }`}
-        onClick={() => handleTabClick("subjects")}
-      >
-        Subjects
-      </p>
+
+      {isAdviser && (
+        <p
+          className={`py-4 text-sm font-medium text-muted-foreground transition-colors hover:cursor-pointer hover:text-foreground ${
+            selectedTab === "subjects" &&
+            "border-b-2 border-foreground !text-foreground"
+          }`}
+          onClick={() => handleTabClick("subjects")}
+        >
+          Subjects
+        </p>
+      )}
+
       <p
         className={`py-4 text-sm font-medium text-muted-foreground transition-colors hover:cursor-pointer hover:text-foreground ${
           selectedTab === "schedule" &&
