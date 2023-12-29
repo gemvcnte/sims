6c5440a88716d37f8ee437908893bbcde8cf23dd
ampol <memetitv01@gmail.com>
2023-12-29 19:33:44 +0800

    Fix 101/update sidebar footer (#111)
    * Refactor sidebar footer to include logout
    functionality
    
    Wrap the sidebar footer in a Link component to enable navigation to the
    user profile, add a logout button, and update avatar display. This
    allows users to easily access their profile and log out, enhancing user
    experience.
    
    * Refactor(ClassroomsDropdown): remove "View" in "View My Classes"

--- src/components/layout/Sidebar/components/SidebarContainer/components/SidebarFooter/index.jsx
+++ src/components/layout/Sidebar/components/SidebarContainer/components/SidebarFooter/index.jsx
@@ -1,25 +1,50 @@
 import { Button } from "@/components/ui/button";
 import { SidebarFooterDropdown } from "./SidebarFooterDropdown";
 import { useAuth } from "@/contexts/AuthContext";
+import { Link } from "react-router-dom";
+import { useSidebarContext } from "@/contexts/SidebarContext";
+import { Icon } from "@iconify/react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function SidebarFooter() {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+  const { toggleSidebar } = useSidebarContext();
+
+  const displayText = user?.username || "";
+  const abbreviatedText = displayText.slice(0, 2).toUpperCase();
 
   return (
-    <Button
-      variant="ghost"
-      className="mb-8 flex items-center justify-between py-8"
-    >
-      <section className="flex items-center gap-2">
-        <div className="h-10 w-10 rounded-full bg-foreground"></div>
-        <div className="flex flex-col items-start justify-around">
-          <span>{user?.username || "username"}</span>
-          <span className="text-muted-foreground">{user?.role}</span>
-        </div>
-      </section>
-      <section>
-        <SidebarFooterDropdown />
-      </section>
-    </Button>
+    <Link to="profile" onClick={() => toggleSidebar()}>
+      <Button
+        variant="ghost"
+        className="mb-8 flex w-full items-center justify-between py-8"
+      >
+        <section className="flex items-center gap-2">
+          <Avatar>
+            <AvatarImage src="" alt="avatar" />
+            <AvatarFallback>{abbreviatedText}</AvatarFallback>
+          </Avatar>{" "}
+          <div className="flex flex-col items-start justify-around">
+            <span>
+              {user?.username && user.username.length > 12
+                ? `${user.username.slice(0, 12)}..`
+                : user.username || "username"}
+            </span>
+            <span className="text-muted-foreground">{user?.role}</span>
+          </div>
+        </section>
+        <section>
+          <Button variant="ghost" onClick={() => logout()}>
+            <Icon
+              icon="material-symbols:logout"
+              rotate={2}
+              width="20"
+              height="20"
+              className="z-20 ml-4 text-muted-foreground"
+            />
+          </Button>
+        </section>
+      </Button>
+    </Link>
   );
 }
