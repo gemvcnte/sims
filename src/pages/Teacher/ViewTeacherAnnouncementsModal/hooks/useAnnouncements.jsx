9f99f9070e183da73ed4533497e6cbfcfd32f348
n0wickk <memetitv01@gmail.com>
2023-12-30 14:21:24 +0800

    Add ViewTeacherAnnouncementsModal

---
+++ src/pages/Teacher/ViewTeacherAnnouncementsModal/hooks/useAnnouncements.jsx
@@ -0,0 +1,29 @@
+import { getTeacherAnnouncementsEndpoint } from "@/config/teacherEndpoints";
+import axiosInstance from "@/utils/axios";
+import { useEffect, useState } from "react";
+
+const useAnnouncements = () => {
+  const [announcements, setAnnouncements] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchAnnouncements = async () => {
+      try {
+        const response = await axiosInstance(getTeacherAnnouncementsEndpoint);
+        const data = await response.json();
+        setAnnouncements(data.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAnnouncements();
+  }, []);
+
+  return { announcements, loading, error };
+};
+
+export default useAnnouncements;
