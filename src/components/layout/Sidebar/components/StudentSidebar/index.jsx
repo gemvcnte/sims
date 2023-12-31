bb63b79d6005f91586a8eca4f077e14df164e51d
n0wickk <memetitv01@gmail.com>
2023-12-31 17:26:55 +0800

    Refactor StudentSidebar to use Dialog component for announcing. Add getStudentAnnouncementsEndpoint for fetching announcements.

--- src/components/layout/Sidebar/components/StudentSidebar/index.jsx
+++ src/components/layout/Sidebar/components/StudentSidebar/index.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import SidebarContainer from "../SidebarContainer";
 import SidebarItem from "@/components/layout/Sidebar/components/SidebarContainer/components/SidebarItem";
-import { DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import ViewTeacherAnnouncementsItem from "../TeacherSidebar/ViewTeacherAnnouncementsItem";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Link } from "react-router-dom";
+import ViewStudentAnnouncementsModal from "@/pages/Student/ViewStudentAnnouncementsModal";
 
 export default function StudentSidebar() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleDialogClick = (event) => {
+    // event.preventDefault();
+    setIsDialogOpen(!isDialogOpen);
+  };
+
   return (
     <SidebarContainer>
       <SidebarItem to="dashboard" icon="material-symbols:dashboard">
@@ -20,9 +34,14 @@ export default function StudentSidebar() {
 
       <DropdownMenuSeparator />
 
-      <SidebarItem to="" icon="mingcute:announcement-line">
-        Announcements
-      </SidebarItem>
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogTrigger>
+          <SidebarItem>
+            <Link onClick={handleDialogClick}>View Announcements</Link>
+          </SidebarItem>
+        </DialogTrigger>
+        <ViewStudentAnnouncementsModal onClose={handleDialogClick} />
+      </Dialog>
 
       <SidebarItem to="profile" icon="teenyicons:user-outline">
         Profile
--- src/config/studentEndpoints.jsx
+++ src/config/studentEndpoints.jsx
@@ -2,3 +2,5 @@ const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 export const getStudentProfileEndpoint = `${baseUrl}/student/profile`;
 export const updateStudentProfileEndpoint = `${baseUrl}/student/profile/update`;
+
+export const getStudentAnnouncementsEndpoint = `${baseUrl}/student/get-announcements`;
