5260768c7e15c668b68b9e2fb2596b41ede0e9fd
n0wickk <memetitv01@gmail.com>
2024-01-06 15:48:44 +0800

    Refactor(StudentDashboard): add useStudentProfile to fetch the studentProfile and save it to localStorage onmount

--- src/pages/Student/StudentDashboard/StudentDashboard.jsx
+++ src/pages/Student/StudentDashboard/StudentDashboard.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import Topbar from "@/components/layout/Topbar";
 import DashboardAnnouncementCard from "./partials/DashboardAnnouncementCard";
 import ProfileProgressCard from "./partials/ProfileProgressCard";
+import { useStudentProfile } from "../StudentProfile/components/StudentProfileDisplayAndEditSection/hooks";
 
 export default function StudentDashboard() {
+  const { studentProfile, error, setStudentProfile } = useStudentProfile();
+
   return (
     <main className="w-full">
       <Topbar>DASHBOARD</Topbar>
