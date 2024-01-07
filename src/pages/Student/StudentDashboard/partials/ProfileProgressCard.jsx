b13a79ff0b219baefb0386da736020f0f7a2ec64
n0wickk <memetitv01@gmail.com>
2024-01-07 19:50:40 +0800

    Update the description for profile completion

--- src/pages/Student/StudentDashboard/partials/ProfileProgressCard.jsx
+++ src/pages/Student/StudentDashboard/partials/ProfileProgressCard.jsx
@@ -66,8 +66,8 @@ export default function ProfileProgressCard() {
       <CardHeader>
         Complete your profile
         <span className="text-sm text-muted-foreground">
-          By completing all the details you have a higher chance of being seen
-          by baddies
+          Attain 100% profile completion to ensure that your profile is fully
+          optimized
         </span>
         <span className="flex items-center gap-2">
           {profileCompletionPercentage}%
