d86fb19cc57ac149c19a9ed35ba56e69330b0b1d
n0wickk <memetitv01@gmail.com>
2024-01-08 16:05:43 +0800

    Refactor DashboardAnnouncementCard for Improved Announcement Display
    Improve the user experience by enhancing the announcement display with
    additional informative text, and also update the 'View all' button for
    better visibility and clarity. This helps to ensure that users are
    well-informed about the latest updates.

--- src/pages/Student/StudentDashboard/partials/DashboardAnnouncementCard.jsx
+++ src/pages/Student/StudentDashboard/partials/DashboardAnnouncementCard.jsx
@@ -32,7 +32,13 @@ export default function DashboardAnnouncementCard() {
 
   return (
     <Card className="">
-      <CardHeader>Most Recent Announcement</CardHeader>
+      <CardHeader>
+        Most Recent Announcement
+        <span className="text-sm text-muted-foreground">
+          Stay informed with the latest updates! Check out the most recent
+          announcement
+        </span>
+      </CardHeader>
       <CardContent className="px-4 pb-2">
         {!loading && !error && (
           <>
@@ -50,9 +56,11 @@ export default function DashboardAnnouncementCard() {
 
       <CardFooter>
         <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-          <DialogTrigger>
-            <span onClick={handleDialogClick}>
-              <Button variant="outline">View all</Button>
+          <DialogTrigger className="w-full">
+            <span onClick={handleDialogClick} className="w-full">
+              <Button variant="outline" className="w-full">
+                View announcements
+              </Button>
             </span>
           </DialogTrigger>
           <ViewStudentAnnouncementsModal onClose={handleDialogClick} />
