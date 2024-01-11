972f290e4d5d6b7da0b303160ae9a584a6fdf17b
n0wickk <memetitv01@gmail.com>
2024-01-11 22:13:00 +0800

    Refactor(LoadingSpinner): add top: 0 property

--- src/utils/LoadingSpinner.jsx
+++ src/utils/LoadingSpinner.jsx
@@ -4,9 +4,9 @@ import { SyncLoader } from "react-spinners";
 const LoadingSpinner = () => {
   return (
     <>
-      <div className="absolute z-10 h-[100vh] w-[100vw] bg-black opacity-80"></div>
-      <div className="absolute z-20  flex h-[100vh] w-[100vw] items-center justify-center">
-        <SyncLoader color={"#18b7f7"} loading={true} size={15} />
+      <div className="absolute top-0 z-10 h-[100vh] w-[100vw] bg-black opacity-80"></div>
+      <div className="absolute top-0  z-20 flex h-[100vh] w-[100vw] items-center justify-center">
+        <SyncLoader color={"#7C3AED"} loading={true} size={15} />
       </div>
     </>
   );
