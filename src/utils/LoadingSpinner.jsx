41180d6df5ad0d73533f26eb0df75b1cfa9dfdfe
n0wickk <memetitv01@gmail.com>
2024-01-11 22:30:22 +0800

    Refactor(LoadingSpinner): change "absolute" to "fixed"

--- src/utils/LoadingSpinner.jsx
+++ src/utils/LoadingSpinner.jsx
@@ -4,8 +4,8 @@ import { SyncLoader } from "react-spinners";
 const LoadingSpinner = () => {
   return (
     <>
-      <div className="absolute top-0 z-10 h-[100vh] w-[100vw] bg-black opacity-80"></div>
-      <div className="absolute top-0  z-20 flex h-[100vh] w-[100vw] items-center justify-center">
+      <div className="fixed  top-0 z-[100] h-[100vh] w-[100vw] bg-black opacity-80"></div>
+      <div className="fixed top-0  z-[100] flex h-[100vh] w-[100vw] items-center justify-center">
         <SyncLoader color={"#7C3AED"} loading={true} size={15} />
       </div>
     </>
