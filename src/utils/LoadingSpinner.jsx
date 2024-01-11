b54656aa9b830e2c694e4124ca5ea746d110c58f
ampol <memetitv01@gmail.com>
2024-01-11 22:57:52 +0800

    Fix 86/add universal loading spinner while waiting for servers response (#145)
    * Refactor(LoadingSpinner): add top: 0 property
    
    * Refactor(LoadingSpinner): change "absolute" to "fixed"
    
    * Refactor(LoadingSpinner): disable scroll onmount
    
    * Refactor(LoadingSpinner): add left-0
    
    * Refactor(LoginForm): return a loading spinner while waiting for server response

--- src/utils/LoadingSpinner.jsx
+++ src/utils/LoadingSpinner.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SyncLoader } from "react-spinners";
 
 const LoadingSpinner = () => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "visible";
+    };
+  }, []);
+
   return (
     <>
-      <div className="absolute z-10 h-[100vh] w-[100vw] bg-black opacity-80"></div>
-      <div className="absolute z-20  flex h-[100vh] w-[100vw] items-center justify-center">
-        <SyncLoader color={"#18b7f7"} loading={true} size={15} />
+      <div className="fixed left-0 top-0 z-[100] h-[100vh] w-[100vw] bg-black opacity-80"></div>
+      <div className="fixed left-0 top-0 z-[100] flex h-[100vh] w-[100vw] items-center justify-center">
+        <SyncLoader color={"#7C3AED"} loading={true} size={15} />
       </div>
     </>
   );
