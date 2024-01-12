db045b47579e982f37403a711d14f5baf0ea8f19
n0wickk <memetitv01@gmail.com>
2024-01-12 19:29:44 +0800

    Refactor StudentGrades to Use ClassDetailsProvider
    Wrap the StudentGrades component with ClassDetailsProvider.
    Additionally, modify StudentGradesTable to fetch and display class
    details. This change improves data management in the StudentGrades page.

--- src/pages/Student/StudentGrades/index.jsx
+++ src/pages/Student/StudentGrades/index.jsx
@@ -1,14 +1,15 @@
 import Topbar from "@/components/layout/Topbar";
 import React from "react";
 import StudentGradesTable from "./partials/StudentGradesTable";
+import { ClassDetailsProvider } from "./hooks/ClassDetailsContext";
 
 export default function StudentGrades() {
   return (
-    <>
+    <ClassDetailsProvider>
       <main className="w-full">
         <Topbar>GRADES</Topbar>
         <StudentGradesTable />
       </main>
-    </>
+    </ClassDetailsProvider>
   );
 }
--- src/pages/Student/StudentGrades/partials/StudentGradesTable.jsx
+++ src/pages/Student/StudentGrades/partials/StudentGradesTable.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useClassDetails } from "../hooks/ClassDetailsContext";
 
 export default function StudentGradesTable() {
+  const classDetailsContext = useClassDetails();
+  const { classDetails: fetchedClass, loading } = classDetailsContext;
+  const [classDetails, setClassDetails] = useState(null);
+
+  useEffect(() => {
+    if (fetchedClass && fetchedClass.length > 0) {
+      const sortedClasses = [...fetchedClass].sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+      );
+      setClassDetails(sortedClasses[0]);
+    }
+  }, [fetchedClass]);
+
+  console.log(classDetails);
+
   return <div>grades table</div>;
 }
