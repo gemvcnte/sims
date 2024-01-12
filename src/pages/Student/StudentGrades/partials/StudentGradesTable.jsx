10f4703eb379e03b3bd2ab0ab0e6c44badcc58e5
n0wickk <memetitv01@gmail.com>
2024-01-12 20:16:11 +0800

    Refactor StudentGradesTable to display student-specific grades
    Rewrite StudentGradesTable to dynamically display individual student
    grades for each subject, catering to user-specific data. This
    enhancement improves the user experience by presenting personalized
    grade information.

--- src/pages/Student/StudentGrades/partials/StudentGradesTable.jsx
+++ src/pages/Student/StudentGrades/partials/StudentGradesTable.jsx
@@ -1,7 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useClassDetails } from "../hooks/ClassDetailsContext";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHeader,
+  TableHead,
+  TableRow,
+} from "@/components/ui/table";
+import { useAuth } from "@/contexts/AuthContext";
 
 export default function StudentGradesTable() {
+  const { user } = useAuth();
   const classDetailsContext = useClassDetails();
   const { classDetails: fetchedClass, loading } = classDetailsContext;
   const [classDetails, setClassDetails] = useState(null);
@@ -17,5 +28,57 @@ export default function StudentGradesTable() {
 
   console.log(classDetails);
 
-  return <div>grades table</div>;
+  const getStudentGrades = (subject) => {
+    const student = classDetails?.students.find(
+      (student) => student.lrn === user.lrn,
+    );
+
+    if (!student) {
+      return { p1Grade: "", p2Grade: "" };
+    }
+
+    const gradesArray = subject.grades;
+
+    // Check if gradesArray is an array with at least one element
+    if (Array.isArray(gradesArray) && gradesArray.length > 0) {
+      const grades = gradesArray.find((grade) => grade.lrn === user.lrn);
+
+      // Check if grades is an object with p1Grade and p2Grade properties
+      if (grades && "p1Grade" in grades && "p2Grade" in grades) {
+        return grades;
+      }
+    }
+
+    return { p1Grade: "", p2Grade: "" };
+  };
+
+  const renderSubjects = () => {
+    return classDetails?.subjects.map((subject) => (
+      <TableRow key={subject._id}>
+        <TableCell>{subject.subjectName}</TableCell>
+        <TableCell>{subject.subjectTeacher}</TableCell>
+        <TableCell>{getStudentGrades(subject).p1Grade}</TableCell>
+        <TableCell>{getStudentGrades(subject).p2Grade}</TableCell>
+      </TableRow>
+    ));
+  };
+
+  return (
+    <main className="m-4">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Subject Name</TableHead>
+            <TableHead>Subject Teacher</TableHead>
+            <TableHead>P1 Grade</TableHead>
+            <TableHead>P2 Grade</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>{renderSubjects()}</TableBody>
+
+        <TableFooter></TableFooter>
+      </Table>
+    </main>
+  );
 }
