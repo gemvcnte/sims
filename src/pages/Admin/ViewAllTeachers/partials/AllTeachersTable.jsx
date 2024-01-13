ea00090090f68e5d2b8274e994417f45c60ae0c4
n0wickk <memetitv01@gmail.com>
2024-01-13 22:03:05 +0800

    Refactor(AllTeachersTable): add footer

--- src/pages/Admin/ViewAllTeachers/partials/AllTeachersTable.jsx
+++ src/pages/Admin/ViewAllTeachers/partials/AllTeachersTable.jsx
@@ -178,6 +178,30 @@ const AllTeachersTable = () => {
           </TableBody>
         </Table>
       </div>
+      <footer className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          Total of {""}
+          {table.getFilteredRowModel().rows.length} teacher(s).
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </footer>
     </div>
   );
 };
