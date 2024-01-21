d26fd23ce6f13408c0467db6b82d56987b04ba41
n0wickk <memetitv01@gmail.com>
2024-01-21 09:49:23 +0800

    Refactor AnnouncementCard component to include a Dialog
    The AnnouncementCard component has been refactored to include a Dialog
    component for displaying additional content. This improves user
    experience by providing a more interactive way of accessing additional
    information. The changes enhance the functionality of the component by
    adding a new feature while maintaining existing behavior.

--- src/pages/Teacher/ViewTeacherAnnouncementsModal/AnnouncementCard.jsx
+++ src/pages/Teacher/ViewTeacherAnnouncementsModal/AnnouncementCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Icon } from "@iconify/react";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 
 const AnnouncementCard = ({
   announcement,
@@ -19,23 +20,46 @@ const AnnouncementCard = ({
         </p>
       </div>
     </div>
-    <button
-      className="mt-4 flex justify-center gap-1 text-xs"
-      variant="outline"
-      onClick={() => toggleContent(announcement._id)}
-    >
-      {expandedAnnouncement === announcement._id ? (
-        <>
-          Hide Details
-          <Icon icon="dashicons:arrow-up" rotate={2} />
-        </>
-      ) : (
-        <>
-          Show Details
-          <Icon icon="dashicons:arrow-up" rotate={1} />
-        </>
-      )}
-    </button>
+    <section className="flex items-baseline justify-between text-xs">
+      <button
+        className="group mt-4 flex items-center justify-center gap-1"
+        variant="outline"
+        onClick={() => toggleContent(announcement._id)}
+      >
+        {expandedAnnouncement === announcement._id ? (
+          <>
+            Hide Details
+            <Icon
+              icon="dashicons:arrow-up"
+              rotate={2}
+              className="inline-block transform transition-all duration-300 group-hover:ml-1 group-hover:rotate-[360deg]"
+            />
+          </>
+        ) : (
+          <>
+            Show Details
+            <Icon
+              icon="dashicons:arrow-up"
+              rotate={1}
+              className="inline-block transform transition-all duration-300 group-hover:rotate-[360deg]"
+            />
+          </>
+        )}
+      </button>
+
+      <Dialog>
+        <DialogTrigger>
+          <button className="group">
+            Update{" "}
+            <Icon
+              icon="octicon:arrow-down-24"
+              rotate={3}
+              className="inline-block -rotate-45 transform transition-all duration-300 group-hover:rotate-45"
+            />
+          </button>
+        </DialogTrigger>
+      </Dialog>
+    </section>
     {expandedAnnouncement === announcement._id && (
       <div className="mt-2 text-sm text-gray-400">{announcement.content}</div>
     )}
