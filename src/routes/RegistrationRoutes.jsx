c3a1a5a2d2c1c70ca21203a868a0cbbf499b7286
ampol <memetitv01@gmail.com>
2024-01-21 20:40:29 +0800

    Fix 160/fix registration page (#164)
    * Refactor(AdminRouter): comment out /registration route
    
    * Refactor(RegistrationRoutes): only return registration route if the user.role is admin

--- src/routes/RegistrationRoutes.jsx
+++ src/routes/RegistrationRoutes.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Registration from "../pages/Registration";
+import { useAuth } from "@/contexts/AuthContext";
 
 const RegistrationRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/registration" element={<Registration />} />
-    </Routes>
-  );
+  const { user } = useAuth();
+  if (user?.role === "admin") {
+    return (
+      <Routes>
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default RegistrationRoutes;
