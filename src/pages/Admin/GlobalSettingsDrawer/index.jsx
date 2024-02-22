83d5ae4e68ab94bc694142b437517c8eb0c0af0d
n0wickk <memetitv01@gmail.com>
2024-02-22 22:38:57 +0800

    Refactor(GlobalSettingsDrawer): update text and style elements

--- src/pages/Admin/GlobalSettingsDrawer/index.jsx
+++ src/pages/Admin/GlobalSettingsDrawer/index.jsx
@@ -10,18 +10,69 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
 
 export function GlobalSetttingsDrawer() {
   return (
     <DrawerContent>
       <div className="mx-auto w-full max-w-sm">
         <DrawerHeader>
-          <DrawerTitle>Move Goal</DrawerTitle>
-          <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+          <DrawerTitle>School Year and Semester</DrawerTitle>
+          <DrawerDescription>
+            Set the current school year and semester
+          </DrawerDescription>
         </DrawerHeader>
 
+        <section className="flex flex-col gap-4 p-4">
+          <div>
+            <Label className="font-semibold">School Year</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select school year" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>School Year</SelectLabel>
+                  <SelectItem value="2023-2024">2023-2024</SelectItem>
+                  <SelectItem value="2024-2025">2024-2025</SelectItem>
+                  <SelectItem value="2025-2026">2025-2026</SelectItem>
+                  <SelectItem value="2026-2027">2026-2027</SelectItem>
+                  <SelectItem value="2028-2029">2028-2029</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div>
+            <Label className="font-semibold">Semester</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select semester" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>School Year</SelectLabel>
+                  <SelectItem value="first semester">First Semester</SelectItem>
+                  <SelectItem value="second semester">
+                    Second Semester
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </section>
+
         <DrawerFooter>
-          <Button>Submit</Button>
+          <Button>Change</Button>
           <DrawerClose asChild>
             <Button variant="outline">Cancel</Button>
           </DrawerClose>
