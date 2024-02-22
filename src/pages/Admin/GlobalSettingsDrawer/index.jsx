f174aca3e1ad6e016bbc13b5109ba4677279f485
n0wickk <memetitv01@gmail.com>
2024-02-22 22:14:47 +0800

    Refactor(GlobalSettingsDrawer): add a drawer for global settings

---
+++ src/pages/Admin/GlobalSettingsDrawer/index.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { Minus, Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+export function GlobalSetttingsDrawer({ children }) {
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>{children} </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Move Goal</DrawerTitle>
+            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+          </DrawerHeader>
+
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
