af71ab06fc5804844da5382f865d522df1b3d937
n0wickk <memetitv01@gmail.com>
2024-03-03 13:09:56 +0800

    Refactor(StudentApplicationModel): update the schoolyear

--- server/models/StudentApplicationModel.js
+++ server/models/StudentApplicationModel.js
@@ -1,134 +1,130 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const studentApplicationSchema = new mongoose.Schema(
-    {
-      firstName: {
-        type: String,
-        required: true,
-        uppercase: true,
-      },
-      lastName: {
-        type: String,
-        required: true,
-        uppercase: true,
-      },
-      middleName: {
-        type: String,
-        default: null,
-        uppercase: true,
-      },
-      extensionName: {
-        type: String,
-        required: true,
-        uppercase: true,
-      },
-      birthDate: {
-        type: String,
-        required: true,
-        uppercase: true,
-      },
-      gender: {
-        type: String,
-        required: true,
-        uppercase: true,
-      },
-      currentAddress: {
-        type: String,
-        required: true,
-        uppercase: true,
-      },
-      emailAddress: {
-        type: String,
-        required: true,
-        trim: true,
-        validate(value) {
-          if (!value.match(/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/)) {
-            throw new Error("Email is not valid.");
-          }
-        },
-      },
-      fatherName: {
-        type: String,
-        uppercase: true,
-      },
-      fatherContactNumber: {
-        type: String,
-        uppercase: true,
-      },
-      motherName: {
-        type: String,
-        uppercase: true,
-      },
-      motherContactNumber: {
-        type: String,
-        uppercase: true,
-      },
-      guardianName: {
-        type: String,
-        default: "none",
-        uppercase: true,
-      },
-      guardianContactNumber: {
-        type: String,
-        default: "none",
-      },
-      guardianAddress: {
-        type: String,
-        default: "none",
-        uppercase: true,
-      },
-      lrn: {
-        type: String,
-        required: true,
-      },
-      schoolYear: {
-        type: String,
-        required: true,
-      },
-      semester: {
-        type: String,
-        required: true,
-        uppercase: true,
-      },
-      gradeLevel: {
-        type: Number,
-        required: true,
-      },
-      track: {
-        type: String,
-        required: true,
-        uppercase: true,
-      },
-      strand: {
-        type: String,
-        required: true,
-        uppercase: true,
-      },
-      registrationDate: String,
-      status: {
-        type: String,
-        default: "pending",
-        uppercase: true,
-      },
-      userImage: {
-        type: String,
-        // need nalang lagyan ng image
-        default: ''
-      },
-  
-      //if want to hash the password only remove the password attribute
-      // password: {
-      //     type: String,
-      //     required: true,
-      // }
+  {
+    firstName: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
-  );
+    middleName: {
+      type: String,
+      default: null,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    extensionName: {
+      type: String,
+      required: true,
+    },
+    birthDate: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    currentAddress: {
+      type: String,
+      required: false,
+    },
+    lrn: {
+      type: String,
+      required: true,
+    },
+    schoolYear: {
+      year: {
+        type: String,
+        required: true,
+      },
+      semester: [
+        {
+          type: {
+            type: String,
+            required: true,
+          },
+          sectionId: {
+            type: mongoose.Types.ObjectId,
+            ref: "Classroom",
+          },
+          sectionName: {
+            type: String,
+            required: true,
+          },
+          gradeLevel: {
+            type: Number,
+            required: true,
+          },
+          track: {
+            type: String,
+            required: false,
+          },
+          strand: {
+            type: String,
+            required: false,
+          },
+        },
+      ],
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!value.match(/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/)) {
+          throw new Error("Email is not valid.");
+        }
+      },
+    },
+    fatherName: {
+      type: String,
+    },
+    fatherContactNumber: {
+      type: String,
+    },
+    motherName: {
+      type: String,
+    },
+    motherContactNumber: {
+      type: String,
+    },
+    guardianName: {
+      type: String,
+      default: "",
+    },
+    guardianContactNumber: {
+      type: String,
+      default: "",
+    },
+    guardianRelationship: {
+      type: String,
+      default: "",
+    },
+    registrationDate: String,
+    status: {
+      type: String,
+      default: "pending",
+      uppercase: true,
+    },
+    userImage: {
+      type: String,
+      // need nalang lagyan ng image
+      default: "",
+    },
 
+    //if want to hash the password only remove the password attribute
+    // password: {
+    //     type: String,
+    //     required: true,
+    // }
+  },
+  { timestamps: true }
+);
 
+const StudentApplication = mongoose.model(
+  "StudentApplication",
+  studentApplicationSchema
+);
 
-const StudentApplication = mongoose.model("StudentApplication", studentApplicationSchema);
-
-
-module.exports = {StudentApplication}
+module.exports = { StudentApplication };
