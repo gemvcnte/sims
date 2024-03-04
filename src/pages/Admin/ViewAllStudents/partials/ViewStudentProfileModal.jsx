22cbdd981430573e9a62b3e36ed7e147b1cdda82
n0wickk <memetitv01@gmail.com>
2024-03-05 01:16:18 +0800

    Refactor(ViewStudentProfileModal): fix the 2029-2030 option value

--- src/pages/Admin/ViewAllStudents/partials/ViewStudentProfileModal.jsx
+++ src/pages/Admin/ViewAllStudents/partials/ViewStudentProfileModal.jsx
@@ -325,7 +325,7 @@ export default function ViewStudentProfileModal({
               <option value="2026-2027">2026-2027</option>
               <option value="2027-2028">2027-2028</option>
               <option value="2028-2029">2028-2029</option>
-              <option value="2029-20230">2029-2030</option>
+              <option value="2029-2030">2029-2030</option>
             </select>
           </div>
 
