11cc98c2475281342a26ba0b6ae5b25c64d46b97
n0wickk <memetitv01@gmail.com>
2024-03-05 13:44:57 +0800

    add custom hook for handling students in specific  class

---
+++ src/pages/Teacher/TeacherSpecificClass/partials/StudentsTable/useStudentsInSpecificClass.jsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
+
+const StudentsInSpecificClassContext = createContext();
+
+export const useStudentsInSpecificClass = () => {
+  const context = useContext(StudentsInSpecificClassContext);
+  if (!context) {
+    throw new Error(
+      "useStudentsInSpecificClass must be used within a StudentsInSpecificClassProvider",
+    );
+  }
+  return context;
+};
+
+export const StudentsInSpecificClassProvider = ({ children }) => {
+  const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchStudentsInSpecificClass = async (sectionId) => {
+    setLoading(true);
+
+    try {
+      const response = await axios.post(`/students-in-specific-class`, {
+        sectionId,
+      });
+      setStudents(response.data.students);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const refetchStudentsInSpecificClass = () => {
+    fetchStudentsInSpecificClass(); // Re-fetch data
+  };
+
+  return (
+    <StudentsInSpecificClassContext.Provider
+      value={{
+        students,
+        loading,
+        error,
+        fetchStudentsInSpecificClass,
+        refetchStudentsInSpecificClass,
+      }}
+    >
+      {children}
+    </StudentsInSpecificClassContext.Provider>
+  );
+};
