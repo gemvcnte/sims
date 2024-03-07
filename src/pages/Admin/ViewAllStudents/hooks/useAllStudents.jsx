9b7ef03a19532202bf259354e16a80e025128463
n0wickk <memetitv01@gmail.com>
2024-03-07 13:36:25 +0800

    Refactor(useAllStudents): add filterStudents function

--- src/pages/Admin/ViewAllStudents/hooks/useAllStudents.jsx
+++ src/pages/Admin/ViewAllStudents/hooks/useAllStudents.jsx
@@ -3,6 +3,7 @@ import axiosInstance from "@/utils/axios";
 import { useEffect, useState } from "react";
 
 export default function useAllStudents() {
+  const [originalAllStudents, setOriginalAllStudents] = useState([]);
   const [allStudents, setAllStudents] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -16,6 +17,7 @@ export default function useAllStudents() {
       const response = await axiosInstance.get(getAllStudentsEndpoint);
 
       if (response.status === 200) {
+        setOriginalAllStudents(response.data.data);
         setAllStudents(response.data.data);
       } else {
         setError(response.data.message);
@@ -31,5 +33,47 @@ export default function useAllStudents() {
     fetchStudents();
   };
 
-  return { allStudents, refetchStudents, loading, error };
+  const filterStudents = ({ schoolYear, semester, gradeLevel, strand }) => {
+    let filtered = originalAllStudents;
+
+    if (schoolYear !== "all") {
+      filtered = filtered.filter((application) =>
+        application.schoolYear.some(
+          (schoolYearItem) => schoolYearItem.year === schoolYear,
+        ),
+      );
+    }
+
+    if (semester !== "all") {
+      filtered = filtered.filter((application) =>
+        application.schoolYear.some(
+          (schoolYearItem) => schoolYearItem.semester === semester,
+        ),
+      );
+    }
+
+    if (gradeLevel !== "") {
+      const gradeLevelNum = parseInt(gradeLevel, 10);
+      console.log("Filter Grade Level:", gradeLevelNum); // Log the filter value
+      filtered = filtered.filter((application) =>
+        application.schoolYear.some((schoolYearItem) => {
+          console.log("Data Grade Level:", schoolYearItem.gradeLevel); // Log the data value
+          return schoolYearItem.gradeLevel === gradeLevelNum;
+        }),
+      );
+    }
+
+    if (strand !== "all") {
+      filtered = filtered.filter((application) =>
+        application.schoolYear.some(
+          (schoolYearItem) => schoolYearItem.strand === strand,
+        ),
+      );
+    }
+
+    console.log(filtered);
+    setAllStudents(filtered);
+  };
+
+  return { allStudents, filterStudents, refetchStudents, loading, error };
 }
