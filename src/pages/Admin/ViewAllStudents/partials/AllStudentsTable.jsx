b67dc17a8017d184b43748f6bfff9838db3240bd
n0wickk <memetitv01@gmail.com>
2024-03-07 13:44:03 +0800

    Refactor(AllStudentsTable): add refetchData function

--- src/pages/Admin/ViewAllStudents/partials/AllStudentsTable.jsx
+++ src/pages/Admin/ViewAllStudents/partials/AllStudentsTable.jsx
@@ -35,9 +35,19 @@ import useAllStudents from "../hooks/useAllStudents";
 import { Drawer, DrawerTrigger } from "@/components/ui/drawer";
 import PendingFiltersDrawer from "../../ApplicationMonitoring/components/PendingApplicationsDataTable/PendingFiltersDrawer";
 import StudentsFiltersDrawer from "./StudentsFiltersDrawer";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const AllStudentsTable = () => {
-  const { allStudents, loading, error } = useAllStudents();
+  const { allStudents, refetchStudents, loading, error } = useAllStudents();
+
+  const refetchData = () => {
+    refetchStudents();
+  };
 
   const columns = [
     {
@@ -127,6 +137,40 @@ const AllStudentsTable = () => {
           </DrawerTrigger>
           <StudentsFiltersDrawer />
         </Drawer>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                variant="outline"
+                onClick={() => {
+                  refetchData();
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15"
+                  height="15"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-refresh-cw"
+                >
+                  <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
+                  <path d="M21 3v5h-5" />
+                  <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16" />
+                  <path d="M8 16H3v5" />
+                </svg>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Reload Data</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
 
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
