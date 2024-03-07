69658c16538df68abb6b650f71f43183d84650f9
n0wickk <memetitv01@gmail.com>
2024-03-07 13:25:08 +0800

    Refactor(useAllStudents): add refetch function

--- src/pages/Admin/ViewAllStudents/hooks/useAllStudents.jsx
+++ src/pages/Admin/ViewAllStudents/hooks/useAllStudents.jsx
@@ -8,24 +8,28 @@ export default function useAllStudents() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchStudents = async () => {
-      try {
-        const response = await axiosInstance.get(getAllStudentsEndpoint);
-
-        if (response.status === 200) {
-          setAllStudents(response.data.data);
-        } else {
-          setError(response.data.message);
-        }
-      } catch (error) {
-        setError("An error occurred while fetching data.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchStudents();
   }, []);
 
-  return { allStudents, loading, error };
+  const fetchStudents = async () => {
+    try {
+      const response = await axiosInstance.get(getAllStudentsEndpoint);
+
+      if (response.status === 200) {
+        setAllStudents(response.data.data);
+      } else {
+        setError(response.data.message);
+      }
+    } catch (error) {
+      setError("An error occurred while fetching data.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const refetchStudents = () => {
+    fetchStudents();
+  };
+
+  return { allStudents, refetchStudents, loading, error };
 }
