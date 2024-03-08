f9f0339e46e9d6f3073ed8741c10e634f38ea0ed
n0wickk <memetitv01@gmail.com>
2024-03-08 21:35:57 +0800

    Refactor(AllStudentsTableSkeleton" change w of skeletons and add breakpoint

--- src/pages/Admin/ViewAllStudents/partials/AllStudentsTableSkeleton.jsx
+++ src/pages/Admin/ViewAllStudents/partials/AllStudentsTableSkeleton.jsx
@@ -6,14 +6,14 @@ export default function AllStudentsTableSkeleton() {
     <main className="overflow-hidden px-4">
       <section className="flex justify-between py-4">
         <div className="flex gap-2">
-          <Skeleton className="h-8 w-[30vw]" />
-          <Skeleton className="h-8 w-[6vw]" />
-          <Skeleton className="h-8 w-[4vw]" />
+          <Skeleton className="h-8 w-[40vw] sm:w-[30vw]" />
+          <Skeleton className="h-8 w-[8vw]" />
+          <Skeleton className="h-8 w-[10vw]" />
         </div>
 
         <div className="flex gap-2">
           {/* <Skeleton className="h-8 w-[5vw]" /> */}
-          <Skeleton className="h-8 w-[8vw]" />
+          <Skeleton className="h-8 w-[20vw] sm:w-[8vw]" />
         </div>
       </section>
 
