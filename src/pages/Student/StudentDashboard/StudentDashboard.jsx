4d70e51925f9e496b9435c7722f15b6a43099cff
n0wickk <memetitv01@gmail.com>
2024-03-10 13:16:24 +0800

    Refactor(StudentDashboard): update the topbar text to handle undefined

--- src/pages/Student/StudentDashboard/StudentDashboard.jsx
+++ src/pages/Student/StudentDashboard/StudentDashboard.jsx
@@ -15,7 +15,7 @@ export default function StudentDashboard() {
   return (
     <ClassDetailsProvider>
       <main className="w-full">
-        <Topbar>{`Welcome, ${user?.username}!`}</Topbar>
+        <Topbar>{user?.username ? `Welcome, ${user.username}!` : null}</Topbar>
         <section className="flex flex-col gap-4 p-4 md:flex-row ">
           <ScheduleTable />
           <div className="order-1 flex min-w-[40%] flex-col gap-4 md:order-2 md:pr-4">
