f594fb4262eb51c9e9d43127ae9c8d8db509e49a
n0wickk <memetitv01@gmail.com>
2024-03-23 12:48:53 +0800

    refactor(overviewTable): display the section details

--- src/pages/Admin/AdminSpecificClass/partials/OverviewTable.jsx
+++ src/pages/Admin/AdminSpecificClass/partials/OverviewTable.jsx
@@ -1,5 +1,47 @@
 import React from "react";
+import { useClassDetails } from "../contexts/ClassDetailsContext";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export default function OverviewTable() {
-  return <div>overviewTable</div>;
+  const { classDetails } = useClassDetails();
+
+  if (!classDetails) {
+    return <p>loading...</p>;
+  }
+
+  return (
+    <main className="flex flex-col gap-4 p-4 ">
+      <section className="flex flex-col gap-4 ">
+        <div>
+          <Label>Section Name</Label>
+          <Input value={classDetails.sectionName.toUpperCase()} disabled />
+        </div>
+
+        <div>
+          <Label>Adviser</Label>
+          <Input value={classDetails.adviser} disabled />
+        </div>
+        <div>
+          <Label>Grade Level</Label>
+          <Input value={classDetails.gradeLevel} disabled />
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-4">
+        <div>
+          <Label>Strand</Label>
+          <Input value={classDetails.strand} disabled />
+        </div>
+        <div>
+          <Label>School Year</Label>
+          <Input value={classDetails.schoolYear} disabled />
+        </div>
+        <div>
+          <Label>Semester</Label>
+          <Input value={classDetails.semester} disabled />
+        </div>
+      </section>
+    </main>
+  );
 }
