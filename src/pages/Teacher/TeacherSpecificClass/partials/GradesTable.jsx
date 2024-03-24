93139491542a75993282824bb09176d3d0cf5532
n0wickk <memetitv01@gmail.com>
2024-03-24 14:41:57 +0800

    gradestable is finally workingggggggggggg
     initializing value worked by removing the required validation

--- src/pages/Teacher/TeacherSpecificClass/partials/GradesTable.jsx
+++ src/pages/Teacher/TeacherSpecificClass/partials/GradesTable.jsx
@@ -17,6 +17,33 @@ import axiosInstance from "@/utils/axios";
 import { updateGradesEndpoint } from "@/config/teacherEndpoints";
 import { isClassAdviser } from "../helpers/isClassAdviser";
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const schema = yup.object().shape({
+  p1Grade: yup
+    .number()
+    .typeError("Grade must be a number")
+    // .required("Grade is required")
+    .min(65, "Grade must be at least 65")
+    .max(100, "Grade must be at most 100"),
+  p2Grade: yup
+    .number()
+    .typeError("Grade must be a number")
+    // .required("Grade is required")
+    .min(65, "Grade must be at least 65")
+    .max(100, "Grade must be at most 100"),
+});
+
 export default function GradesTable() {
   const { user } = useAuth();
   const classDetailsContext = useClassDetails();
@@ -27,8 +54,16 @@ export default function GradesTable() {
 
   console.log(classDetails);
 
-  const handleSaveChanges = async () => {
+  const handleSaveChanges = async (e) => {
+    e.preventDefault(); // Prevent default form submission
+
+    // console.log(`modifiedGrades: ${modifiedGrades}`);
+
     try {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+
+      console.log("validation passed");
       const classId = classDetails._id;
       const subjectId = classDetails.subjects.find(
         (subject) => subject.subjectName === selectedSubject,
@@ -88,10 +123,6 @@ export default function GradesTable() {
     }));
   };
 
-  useEffect(() => {
-    initializeModifiedGrades();
-  }, [selectedSubject]);
-
   const initializeModifiedGrades = () => {
     const initialModifiedGrades = {};
 
@@ -112,6 +143,21 @@ export default function GradesTable() {
 
     setModifiedGrades(initialModifiedGrades);
   };
+
+  useEffect(() => {
+    if (filteredSubjects.length > 0) {
+      setSelectedSubject(filteredSubjects[0].subjectName);
+    }
+  }, [filteredSubjects]);
+
+  useEffect(() => {
+    initializeModifiedGrades();
+  }, [selectedSubject]);
+
+  const form = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: modifiedGrades, // Set default values here
+  });
 
   const renderSortedStudents = () => {
     const studentsToRender = classDetails?.students || [];
@@ -140,13 +186,40 @@ export default function GradesTable() {
               <TableCell>{selectedSubject}</TableCell>
               {isEditing ? (
                 <TableCell>
-                  <input
+                  {/* <input
                     type="number"
                     className="border bg-background text-foreground"
                     value={modifiedGrades[student.lrn]?.p1Grade}
                     onChange={(e) =>
                       handleChangeGrade(student.lrn, "p1Grade", e.target.value)
                     }
+                  /> */}
+                  <FormField
+                    control={form.control}
+                    name="p1Grade"
+                    render={({ field }) => (
+                      <FormItem className="space-y-0 py-4">
+                        <FormControl>
+                          <Input
+                            type="number"
+                            className="border bg-background text-foreground"
+                            {...field}
+                            id="p1Grade"
+                            value={modifiedGrades[student.lrn]?.p1Grade}
+                            onChange={(e) => {
+                              e.target.value = e.target.value;
+                              field.onChange(e);
+                              handleChangeGrade(
+                                student.lrn,
+                                "p1Grade",
+                                e.target.value,
+                              );
+                            }}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
                 </TableCell>
               ) : (
@@ -160,13 +233,32 @@ export default function GradesTable() {
               )}
               {isEditing ? (
                 <TableCell>
-                  <input
-                    type="number"
-                    className="border bg-background text-foreground"
-                    value={modifiedGrades[student.lrn]?.p2Grade}
-                    onChange={(e) =>
-                      handleChangeGrade(student.lrn, "p2Grade", e.target.value)
-                    }
+                  <FormField
+                    control={form.control}
+                    name="p2Grade"
+                    render={({ field }) => (
+                      <FormItem className="space-y-0 py-4">
+                        <FormControl>
+                          <Input
+                            type="number"
+                            className="border bg-background text-foreground "
+                            {...field}
+                            id="p2Grade"
+                            value={modifiedGrades[student.lrn]?.p2Grade}
+                            onChange={(e) => {
+                              e.target.value = e.target.value;
+                              field.onChange(e);
+                              handleChangeGrade(
+                                student.lrn,
+                                "p2Grade",
+                                e.target.value,
+                              );
+                            }}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
                 </TableCell>
               ) : (
@@ -187,9 +279,11 @@ export default function GradesTable() {
   };
 
   return (
-    <main className="p-4">
-      <header className="mb-4 flex justify-between gap-4">
-        {/* {isSubjectTeacher ? (
+    <Form {...form}>
+      <form>
+        <main className="p-4">
+          <header className="mb-4 flex justify-between gap-4">
+            {/* {isSubjectTeacher ? (
           <div className="flex gap-4">
             <Button
               variant="outline"
@@ -204,54 +298,57 @@ export default function GradesTable() {
         ) : (
           <div></div>
         )} */}
-        <div className="flex gap-4">
-          <Button
-            disabled={!isSubjectTeacher}
-            variant="outline"
-            onClick={() => setIsEditing((prev) => !prev)}
-          >
-            {isEditing ? "Cancel Editing" : "Edit Grades"}
-          </Button>
-          {isEditing && (
-            <Button onClick={handleSaveChanges}>Save Changes</Button>
-          )}
-        </div>
-        <div>
-          <select
-            onChange={(e) => setSelectedSubject(e.target.value)}
-            value={selectedSubject}
-            className="col-span-3 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-          >
-            <option value="">Select Subject</option>
-            {filteredSubjects.map((subject) => (
-              <option key={subject._id} value={subject.subjectName}>
-                {subject.subjectName}
-              </option>
-            ))}
-          </select>
-        </div>
-      </header>
+            <div className="flex gap-4">
+              <Button
+                disabled={!isSubjectTeacher}
+                type="button"
+                variant="outline"
+                onClick={() => setIsEditing((prev) => !prev)}
+              >
+                {isEditing ? "Cancel Editing" : "Edit Grades"}
+              </Button>
+              {isEditing && (
+                <Button onClick={handleSaveChanges}>Save Changes</Button>
+              )}
+            </div>
+            <div>
+              <select
+                onChange={(e) => setSelectedSubject(e.target.value)}
+                value={selectedSubject}
+                className="col-span-3 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              >
+                <option value="">Select Subject</option>
+                {filteredSubjects.map((subject) => (
+                  <option key={subject._id} value={subject.subjectName}>
+                    {subject.subjectName}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </header>
 
-      <Table>
-        {classDetails?.students.length === 0 && !isEditing && (
-          <TableCaption className="pb-4">No Students Found</TableCaption>
-        )}
+          <Table>
+            {classDetails?.students.length === 0 && !isEditing && (
+              <TableCaption className="pb-4">No Students Found</TableCaption>
+            )}
 
-        <TableHeader>
-          <TableRow>
-            <TableHead>Lastname</TableHead>
-            <TableHead>Firstname</TableHead>
-            <TableHead>LRN</TableHead>
-            <TableHead>Subject</TableHead>
-            <TableHead>QTR1</TableHead>
-            <TableHead>QTR2</TableHead>
-          </TableRow>
-        </TableHeader>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Lastname</TableHead>
+                <TableHead>Firstname</TableHead>
+                <TableHead>LRN</TableHead>
+                <TableHead>Subject</TableHead>
+                <TableHead>QTR1</TableHead>
+                <TableHead>QTR2</TableHead>
+              </TableRow>
+            </TableHeader>
 
-        <TableBody>{renderSortedStudents()}</TableBody>
+            <TableBody>{renderSortedStudents()}</TableBody>
 
-        <TableFooter></TableFooter>
-      </Table>
-    </main>
+            <TableFooter></TableFooter>
+          </Table>
+        </main>
+      </form>
+    </Form>
   );
 }
