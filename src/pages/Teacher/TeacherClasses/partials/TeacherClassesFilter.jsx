74212908040cff58c195ff9549e9bfce8e448923
n0wickk <memetitv01@gmail.com>
2024-04-07 19:43:22 +0800

    feat(TeacherClassesFIlter): add all school years avaialble option

--- src/pages/Teacher/TeacherClasses/partials/TeacherClassesFilter.jsx
+++ src/pages/Teacher/TeacherClasses/partials/TeacherClassesFilter.jsx
@@ -59,7 +59,11 @@ export default function TeacherClassesFilter() {
           <SelectGroup>
             <SelectItem value="all">All School Years</SelectItem>
             <SelectItem value="2023-2024">2023-2024</SelectItem>
+            <SelectItem value="2024-2025">2024-2025</SelectItem>
+            <SelectItem value="2025-2026">2025-2026</SelectItem>
+            <SelectItem value="2026-2027">2026-2027</SelectItem>
             <SelectItem value="2028-2029">2028-2029</SelectItem>
+            <SelectItem value="2029-2030">2029-2030</SelectItem>
           </SelectGroup>
         </SelectContent>
       </Select>
