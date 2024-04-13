6cc1edf093f2c370f4822133018108ee60d95dad
n0wickk <memetitv01@gmail.com>
2024-04-13 10:33:32 +0800

    add topbar skeleton

--- src/pages/Student/StudentGrades/index.jsx
+++ src/pages/Student/StudentGrades/index.jsx
@@ -3,13 +3,21 @@ import React, { useState } from "react";
 import StudentGradesTable from "./partials/StudentGradesTable";
 import { ClassDetailsProvider } from "./hooks/ClassDetailsContext";
 import FilterGrades from "./partials/FilterGrades";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function StudentGrades() {
   const [sectionName, setSectionName] = useState("");
+
   return (
     <ClassDetailsProvider>
       <main className="w-full">
-        <Topbar>GRADES - {sectionName.toUpperCase()}</Topbar>
+        {sectionName ? (
+          <Topbar>GRADES - {sectionName?.toUpperCase()}</Topbar>
+        ) : (
+          <Topbar>
+            <Skeleton className="h-8 w-[30ch]"></Skeleton>
+          </Topbar>
+        )}
 
         <FilterGrades />
         <StudentGradesTable setSectionName={setSectionName} />
