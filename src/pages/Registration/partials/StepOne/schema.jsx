65bf56ea76f852f13d1863802e068f5203178848
n0wickk <memetitv01@gmail.com>
2024-04-14 22:43:44 +0800

    feat(schema): update messages to be more conversational

--- src/pages/Registration/partials/StepOne/schema.jsx
+++ src/pages/Registration/partials/StepOne/schema.jsx
@@ -3,31 +3,31 @@ import * as yup from "yup";
 export const schema = yup.object().shape({
   lastName: yup
     .string()
-    .required("Last name is required")
-    .max(255, "Last name must be at most 255 characters"),
+    .required("Please provide your last name.")
+    .max(255, "Your last name must be at most 255 characters long."),
   firstName: yup
     .string()
-    .required("First name is required")
-    .max(255, "First name must be at most 255 characters"),
+    .required("Please provide your first name.")
+    .max(255, "Your first name must be at most 255 characters long."),
   middleName: yup
     .string()
-    .max(255, "Middle name must be at most 255 characters"),
+    .max(255, "Your middle name must be at most 255 characters long."),
   extensionName: yup.string().default("None"),
   birthDate: yup
     .date()
-    .max(new Date(), "Birthdate cannot be in the future")
-    .required("Date of birth is required"),
-  gender: yup.string().required("Gender is required"),
+    .max(new Date(), "Please enter a birthdate in the past.")
+    .required("Your date of birth is required."),
+  gender: yup.string().required("Please specify your gender."),
   currentAddress: yup
     .string()
-    .required("Current address is required")
-    .max(255, "Current address must be at most 255 characters"),
+    .required("Please provide your current address.")
+    .max(255, "Your current address must be at most 255 characters long."),
   email: yup
     .string()
-    .email("Invalid email")
-    .required("Email is required")
-    .max(255, "Email must be at most 255 characters")
-    .test("email-format", "Invalid email format", (value) => {
+    .email("Please enter a valid email address.")
+    .required("Your email address is required.")
+    .max(255, "Your email address must be at most 255 characters long.")
+    .test("email-format", "Please enter a valid email format.", (value) => {
       return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
     }),
 });
