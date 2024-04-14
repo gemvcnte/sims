6f65cae4d4e82ae9e06bfac03cbb8ded46b46b36
n0wickk <memetitv01@gmail.com>
2024-04-14 17:08:39 +0800

    feat(RegistrationLayout): return message if the user is on mobile

--- src/pages/Registration/layout.jsx
+++ src/pages/Registration/layout.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { EnrollmentProvider } from "./useEnrollment";
 import Header from "./partials/Header";
 
 export default function EnrollmentLayout({ children }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+
+    checkIsMobile();
+
+    window.addEventListener("resize", checkIsMobile);
+
+    return () => {
+      window.removeEventListener("resize", checkIsMobile);
+    };
+  }, []);
+
+  if (isMobile) {
+    return (
+      <main className="flex h-[100svh] items-center justify-center px-4">
+        <p className="text-center">
+          Our Enrollment form is only accessible through the MRMNHS computer
+          lab.
+          <br />
+          <br />
+          <br />
+          If you need assistance, feel free to reach out to our friendly
+          administrators! 🤝
+        </p>
+      </main>
+    );
+  }
+
   return (
     <EnrollmentProvider>
       <Header />
