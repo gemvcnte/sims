49825980064943bbcdc8d6908f8c2e621767802e
ampol <memetitv01@gmail.com>
2024-04-20 07:24:37 +0800

    Fix2 388/admin analytics student schema (#391)
    * feat(StudentsTabContent): return loading whiel loading
    
    * fix(FacultyTabContext): add loading while loading

--- src/pages/Admin/AnalyticsDrawer/partials/FacultyTabContent/index.jsx
+++ src/pages/Admin/AnalyticsDrawer/partials/FacultyTabContent/index.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import TotalFacultyCard from "./TotalFacultyCard";
 import TotalTeachersCard from "./TotalTeachersCard";
 import TotalAdminsCard from "./TotalAdminsCard";
+import { useAnalyticsContext } from "../../context/AnalyticsContext";
 
 export default function FacultyTabContent() {
+  const { loading } = useAnalyticsContext();
+
+  if (loading) {
+    return <p>loading...</p>;
+  }
+
   return (
     <main className="flex w-full flex-col gap-2">
       <TotalFacultyCard />
