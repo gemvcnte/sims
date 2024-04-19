ece25b65916e63059dfb2efaef5db62d1dabe7dc
n0wickk <memetitv01@gmail.com>
2024-04-19 20:56:32 +0800

    feat(SidebarHeader): add logo and logo-text

--- src/components/layout/Sidebar/components/SidebarContainer/components/SidebarHeader/index.jsx
+++ src/components/layout/Sidebar/components/SidebarContainer/components/SidebarHeader/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+// import logo from "@/assets/sims-logo.svg";
+import logo from "@/assets/sims-logo-img.svg";
+import logoText from "@/assets/sims-logo-text.svg";
 
 export default function SidebarHeader({}) {
   return (
     <header className="border-white-700 border-b py-8 text-center italic">
-      <span>SIMS v1.0</span>
+      {/* <span>SIMS v1.0</span> */}
+      <div className="mx-auto flex max-w-[50%] gap-1">
+        <img src={logo} alt="SIMS logo" className="max-w-[5ch]" />
+        <img src={logoText} alt="SIMS logo" className="max-w-[10ch]" />
+      </div>
     </header>
   );
 }
