ab8aa76e203edea9e8d4f9befa5f1d8bbb6fa339
n0wickk <memetitv01@gmail.com>
2024-04-20 13:03:44 +0800

    fix(SidebarHeader): fix logo py alignment

--- src/components/layout/Sidebar/components/SidebarContainer/components/SidebarHeader/index.jsx
+++ src/components/layout/Sidebar/components/SidebarContainer/components/SidebarHeader/index.jsx
@@ -4,7 +4,7 @@ import logoText from "@/assets/sims-logo-text.svg";
 
 export default function SidebarHeader({}) {
   return (
-    <header className="border-white-700 border-b py-8 text-center italic">
+    <header className="border-white-700 border-b py-6 text-center italic">
       <div className="mx-auto flex max-w-[50%] gap-1">
         <img src={logo} alt="SIMS logo" className="max-w-[5ch]" />
         <img src={logoText} alt="SIMS logo" className="max-w-[10ch]" />
