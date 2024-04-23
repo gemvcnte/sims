30c0079933a9f4f7851a24019c80b26c8544df5d
ampol <memetitv01@gmail.com>
2024-04-23 08:10:57 +0800

    Fix 403/feature schedule UI (#409)
    * feat: ScheduleTable use react-big-calendar instead of shadcn table
    
    * style(SidebarItem): replace text-background with text-white
    
    * style(index.css): add custom styles to rbc elements
    
    * feat(student-dashboard-schedule): replace table to use react-big-calendar
    
    * feat(teacher-specific-class-schedule): use react-big-calendar
    
    * feat(admin-specific-class): use react big calendar

--- src/pages/Admin/AdminSpecificClass/partials/ScheduleTable.jsx
+++ src/pages/Admin/AdminSpecificClass/partials/ScheduleTable.jsx
@@ -1,116 +1,100 @@
-import React, { useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import React, { useEffect, useState } from "react";
 import { useClassDetails } from "../contexts/ClassDetailsContext";
-import AddSubjectModal from "./AddSubjectModal";
-import UpdateSubjectModal from "./UpdateSubjectModal";
-import { Icon } from "@iconify/react";
-import { DeleteSubjectModal } from "./DeleteSubjectModal";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import AdminScheduleSkeleton from "@/pages/Admin/AdminSchedule/index.skeleton";
+
+const localizer = momentLocalizer(moment);
+
+const minimumStartTime7am = new Date();
+minimumStartTime7am.setHours(7, 0, 0);
+
+const maximumEndTime6pm = new Date();
+maximumEndTime6pm.setHours(18, 0, 0);
 
 export default function ScheduleTable() {
   const classDetailsContext = useClassDetails();
   const { classDetails, loading, fetchClassDetails } = classDetailsContext;
 
+  if (loading) {
+    return <AdminScheduleSkeleton />;
+  }
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  const checkIsMobile = () => {
+    const width = window.innerWidth;
+    setIsMobile(width <= 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkIsMobile);
+    return () => {
+      window.removeEventListener("resize", checkIsMobile);
+    };
+  }, []);
+
+  const generateEvents = () => {
+    const events = [];
+
+    if (classDetails && classDetails.subjects) {
+      classDetails.subjects.forEach((subject) => {
+        subject.schedules.forEach((schedule) => {
+          if (
+            ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].includes(
+              schedule.day,
+            )
+          ) {
+            events.push({
+              id: `${classDetails._id}-${subject._id}-${schedule._id}`,
+              title: isMobile
+                ? abbreviateSubject(subject.subjectName)
+                : subject.subjectName,
+              start: moment()
+                .day(schedule.day)
+                .hour(Number(schedule.startTime.split(":")[0]))
+                .minute(Number(schedule.startTime.split(":")[1]))
+                .toDate(),
+              end: moment()
+                .day(schedule.day)
+                .hour(Number(schedule.endTime.split(":")[0]))
+                .minute(Number(schedule.endTime.split(":")[1]))
+                .toDate(),
+              allDay: false,
+            });
+          }
+        });
+      });
+    }
+
+    return events;
+  };
+
+  const abbreviateSubject = (subjectName) => {
+    const words = subjectName.split(" ");
+    return words.map((word) => word[0]?.toUpperCase()).join(".");
+  };
+
+  const dayFormat = (date, culture, localizer) =>
+    localizer.format(date, "ddd").toUpperCase();
+
   return (
     <main className="p-4">
-      <Table>
-        {classDetails.subjects.length === 0 && (
-          <TableCaption className="pb-4">No Subjects Found</TableCaption>
-        )}
-        <TableHeader>
-          <TableRow>
-            <TableHead></TableHead>
-            <TableHead className="text-center">Monday</TableHead>
-            <TableHead className="text-center">Tuesday</TableHead>
-            <TableHead className="text-center">Wednesday</TableHead>
-            <TableHead className="text-center">Thursday</TableHead>
-            <TableHead className="text-center">Friday</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {Array.from({ length: 12 }, (_, index) => index + 7).map((hour) => (
-            <TableRow key={hour}>
-              <TableCell className="font-medium">
-                {hour < 12
-                  ? `${hour === 0 ? 12 : hour}:00 AM`
-                  : hour === 12
-                    ? `12:00 PM`
-                    : `${hour - 12}:00 PM`}
-              </TableCell>
-              {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].map(
-                (day) => {
-                  const subjectForDayAndHour = classDetails.subjects.find(
-                    (subject) => {
-                      const subjectSchedule = subject.schedules.find(
-                        (schedule) =>
-                          schedule.day === day &&
-                          parseInt(schedule.startTime.split(":")[0]) === hour,
-                      );
-                      return subjectSchedule;
-                    },
-                  );
-
-                  return (
-                    <TableCell className="text-center" key={`${day}-${hour}`}>
-                      {subjectForDayAndHour ? (
-                        <div>
-                          <p>{subjectForDayAndHour.subjectName}</p>
-                          <p>
-                            {convertTo12HourFormat(
-                              subjectForDayAndHour.schedules
-                                .find(
-                                  (schedule) =>
-                                    schedule.day === day &&
-                                    parseInt(
-                                      schedule.startTime.split(":")[0],
-                                    ) === hour,
-                                )
-                                .startTime.replace(/^0/, ""),
-                            )}{" "}
-                            <br className="sm:hidden" />-
-                            <br className="sm:hidden" />{" "}
-                            {convertTo12HourFormat(
-                              subjectForDayAndHour.schedules
-                                .find(
-                                  (schedule) =>
-                                    schedule.day === day &&
-                                    parseInt(
-                                      schedule.startTime.split(":")[0],
-                                    ) === hour,
-                                )
-                                .endTime.replace(/^0/, ""),
-                            )}
-                          </p>
-                        </div>
-                      ) : (
-                        ""
-                      )}
-                    </TableCell>
-                  );
-                },
-              )}
-            </TableRow>
-          ))}
-        </TableBody>
-
-        <TableFooter></TableFooter>
-      </Table>
+      <Calendar
+        localizer={localizer}
+        events={generateEvents()}
+        formats={{ dayFormat }}
+        min={minimumStartTime7am}
+        max={maximumEndTime6pm}
+        step={15}
+        startAccessor="start"
+        endAccessor="end"
+        toolbar={false}
+        views={{ work_week: true }}
+        defaultView="work_week"
+        titleAccessor={"title"}
+      />
     </main>
   );
-}
-
-function convertTo12HourFormat(time) {
-  const [hours, minutes] = time.split(":");
-  const period = hours >= 12 ? "PM" : "AM";
-  const formattedHours = hours % 12 || 12;
-  //   return `${formattedHours}:${minutes} ${period}`;
-  return `${formattedHours}:${minutes}`;
 }
--- src/pages/Student/StudentSchedule/partials/ScheduleTable.jsx
+++ src/pages/Student/StudentSchedule/partials/ScheduleTable.jsx
@@ -1,120 +1,209 @@
-import React, { useEffect, useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+// import React, { useEffect, useState } from "react";
+// import { Calendar, momentLocalizer } from "react-big-calendar";
+// import moment from "moment";
+// import "react-big-calendar/lib/css/react-big-calendar.css";
+// import { useClassDetails } from "../contexts/ClassDetailsContext";
+// import AdminScheduleSkeleton from "@/pages/Admin/AdminSchedule/index.skeleton";
+
+// const localizer = momentLocalizer(moment);
+
+// export default function ScheduleTable() {
+//   const classDetailsContext = useClassDetails();
+//   const { classDetails: fetchedClassDetails, loading } = classDetailsContext;
+
+//   const [events, setEvents] = useState([]);
+
+//   console.log(`fetchedClassDetails`, fetchedClassDetails);
+
+//   useEffect(() => {
+//     if (!loading && fetchedClassDetails.length > 0) {
+//       console.log(`fetchedClassDetails`, fetchedClassDetails);
+
+//       console.log(
+//         `   fetchedClassDetails[0].subjects`,
+//         fetchedClassDetails[0].subjects,
+//       );
+
+//       const transformedEvents = transformEvents(
+//         fetchedClassDetails[0].subjects,
+//       );
+//       setEvents(transformedEvents);
+//       console.log(`transformedEvents`, transformedEvents);
+//     }
+//   }, [fetchedClassDetails, loading]);
+
+//   useEffect(() => {
+//     console.log(`events`, events);
+//   }, [events]);
+
+//   if (loading) {
+//     return <AdminScheduleSkeleton />;
+//   }
+
+//   return (
+//     <main className="p-4">
+//       <Calendar
+//         localizer={localizer}
+//         // events={events}
+//         // startAccessor="startTime"
+//         // endAccessor="endTime"
+//         // views={["week"]}
+//         style={{ height: 500 }}
+//       />
+//     </main>
+//   );
+// }
+
+// function transformEvents(classDetails) {
+//   const events = [];
+
+//   const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+//   classDetails.forEach((classDetail) => {
+//     if (classDetail.subjects && Array.isArray(classDetail.subjects)) {
+//       classDetail.subjects.forEach((subject) => {
+//         if (subject.schedules && Array.isArray(subject.schedules)) {
+//           subject.schedules.forEach((schedule) => {
+//             const dayOfWeek = schedule.day;
+//             const startTime = moment(schedule.startTime, "HH:mm");
+//             const endTime = moment(schedule.endTime, "HH:mm");
+
+//             // If the day of the week from the schedule matches one of the days in daysOfWeek
+//             if (daysOfWeek.includes(dayOfWeek)) {
+//               const event = {
+//                 id: `${classDetail._id}-${subject._id}-${schedule._id}`,
+//                 title: "test",
+//                 start: startTime.toDate(),
+//                 end: endTime.toDate(),
+//                 // Add an extra property to store the day of the week
+//                 dayOfWeek: dayOfWeek,
+//               };
+//               events.push(event);
+//             }
+//           });
+//         }
+//       });
+//     }
+//   });
+
+//   return events;
+// }
+
+import React, { useState, useEffect } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useClassDetails } from "../contexts/ClassDetailsContext";
 import AdminScheduleSkeleton from "@/pages/Admin/AdminSchedule/index.skeleton";
 
-export default function ScheduleTable() {
+const localizer = momentLocalizer(moment);
+
+const minTime = new Date();
+minTime.setHours(7, 0, 0); // Minimum time set to 7am
+
+const maxTime = new Date();
+maxTime.setHours(18, 0, 0); // Maximum time set to 6pm
+
+const ScheduleTable = () => {
   const classDetailsContext = useClassDetails();
   const { classDetails: fetchedClassDetails, loading } = classDetailsContext;
+
+  // State to track device width
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Function to check if the device width is less than or equal to a certain threshold
+  const checkIsMobile = () => {
+    const width = window.innerWidth;
+    setIsMobile(width <= 768); // Adjust threshold as needed
+  };
+
+  useEffect(() => {
+    // Add event listener on component mount to check device width
+    window.addEventListener("resize", checkIsMobile);
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", checkIsMobile);
+    };
+  }, []);
 
   if (loading) {
     return <AdminScheduleSkeleton />;
   }
 
-  const classDetails = fetchedClassDetails[0];
+  // Function to generate events dynamically for all Mondays and Fridays
+  const generateEvents = () => {
+    const events = [];
+    const firstClassDetail = fetchedClassDetails[0]; // Get the first class detail
+
+    // Check if the first class detail exists and has subjects
+    if (firstClassDetail && firstClassDetail.subjects) {
+      // Loop through subjects array of the first class detail
+      firstClassDetail.subjects.forEach((subject) => {
+        // Loop through schedules array for each subject
+        subject.schedules.forEach((schedule) => {
+          // Check if the schedule day is Monday, Tuesday, Wednesday, Thursday, or Friday
+          if (
+            ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].includes(
+              schedule.day,
+            )
+          ) {
+            // Push event for the subject with appropriate start and end times
+            events.push({
+              id: `${firstClassDetail._id}-${subject._id}-${schedule._id}`,
+              title: isMobile
+                ? abbreviateSubject(subject.subjectName)
+                : subject.subjectName,
+              start: moment()
+                .day(schedule.day)
+                .hour(Number(schedule.startTime.split(":")[0]))
+                .minute(Number(schedule.startTime.split(":")[1]))
+                .toDate(),
+              end: moment()
+                .day(schedule.day)
+                .hour(Number(schedule.endTime.split(":")[0]))
+                .minute(Number(schedule.endTime.split(":")[1]))
+                .toDate(),
+              allDay: false,
+            });
+          }
+        });
+      });
+    }
+
+    return events;
+  };
+
+  // Function to abbreviate subject title
+  const abbreviateSubject = (subjectName) => {
+    // Split subject name by space
+    const words = subjectName.split(" ");
+    // Take the first character of each word and join them
+    return words.map((word) => word[0]?.toUpperCase()).join(".");
+  };
+
+  const dayFormat = (date, culture, localizer) =>
+    localizer.format(date, "ddd").toUpperCase();
 
   return (
-    <main className="p-4">
-      <Table>
-        {!classDetails && (
-          <TableCaption className="pb-6 pt-4">No Schedule Found</TableCaption>
-        )}
-        <TableHeader>
-          <TableRow>
-            <TableHead></TableHead>
-            <TableHead className="text-center">Monday</TableHead>
-            <TableHead className="text-center">Tuesday</TableHead>
-            <TableHead className="text-center">Wednesday</TableHead>
-            <TableHead className="text-center">Thursday</TableHead>
-            <TableHead className="text-center">Friday</TableHead>
-          </TableRow>
-        </TableHeader>
-        {classDetails && (
-          <TableBody>
-            {Array.from({ length: 12 }, (_, index) => index + 7).map((hour) => (
-              <TableRow key={hour}>
-                <TableCell className="font-medium">
-                  {hour < 12
-                    ? `${hour === 0 ? 12 : hour}:00 AM`
-                    : hour === 12
-                      ? `12:00 PM`
-                      : `${hour - 12}:00 PM`}
-                </TableCell>
-                {["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].map(
-                  (day) => {
-                    const subjectForDayAndHour = classDetails.subjects.find(
-                      (subject) => {
-                        const subjectSchedule = subject.schedules.find(
-                          (schedule) =>
-                            schedule.day === day &&
-                            parseInt(schedule.startTime.split(":")[0]) === hour,
-                        );
-                        return subjectSchedule;
-                      },
-                    );
-
-                    return (
-                      <TableCell className="text-center" key={`${day}-${hour}`}>
-                        {subjectForDayAndHour ? (
-                          <div>
-                            <p>{subjectForDayAndHour.subjectName}</p>
-                            <p>
-                              {convertTo12HourFormat(
-                                subjectForDayAndHour.schedules
-                                  .find(
-                                    (schedule) =>
-                                      schedule.day === day &&
-                                      parseInt(
-                                        schedule.startTime.split(":")[0],
-                                      ) === hour,
-                                  )
-                                  .startTime.replace(/^0/, ""),
-                              )}{" "}
-                              <br className="sm:hidden" />-
-                              <br className="sm:hidden" />{" "}
-                              {convertTo12HourFormat(
-                                subjectForDayAndHour.schedules
-                                  .find(
-                                    (schedule) =>
-                                      schedule.day === day &&
-                                      parseInt(
-                                        schedule.startTime.split(":")[0],
-                                      ) === hour,
-                                  )
-                                  .endTime.replace(/^0/, ""),
-                              )}
-                            </p>
-                          </div>
-                        ) : (
-                          ""
-                        )}
-                      </TableCell>
-                    );
-                  },
-                )}
-              </TableRow>
-            ))}
-          </TableBody>
-        )}
-        <TableFooter></TableFooter>
-      </Table>
-    </main>
+    <div className="p-4">
+      <Calendar
+        localizer={localizer}
+        events={generateEvents()}
+        formats={{ dayFormat }}
+        min={minTime} // Set minimum time
+        max={maxTime} // Set maximum time
+        step={15} // Interval between time slots
+        startAccessor="start" // Start time accessor
+        endAccessor="end" // End time accessor
+        toolbar={false} // Hide the toolbar
+        views={{ work_week: true }} // Show only the work_week view
+        defaultView="work_week" // Default view set to work_week
+        // view="work_week" // Default view set to work_week
+        // Conditionally set title accessor based on device width
+        titleAccessor={isMobile ? "title" : undefined}
+      />
+    </div>
   );
-}
+};
 
-function convertTo12HourFormat(time) {
-  const [hours, minutes] = time.split(":");
-  const period = hours >= 12 ? "PM" : "AM";
-  const formattedHours = hours % 12 || 12;
-  //   return `${formattedHours}:${minutes} ${period}`;
-  return `${formattedHours}:${minutes}`;
-}
+export default ScheduleTable;
