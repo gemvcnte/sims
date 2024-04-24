ac94512c140c5aa746de2eb132997a8c9fa61b42
n0wickk <memetitv01@gmail.com>
2024-04-25 07:39:27 +0800

    add addSCheduleModal

---
+++ src/pages/Teacher/TeacherSpecificClass/partials/AddScheduleModal/AddSubjectModalForm.jsx
@@ -0,0 +1,268 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import SelectTeacherCombobox from "../SelectTeacherCombobox";
+import { Button } from "@/components/ui/button";
+import useAddSubjectModal from "./useAddSubjectModal";
+import { schema } from "./schema";
+import { useCheckOverlap } from "./useCheckOverlap";
+import { BadgePlus, MessageSquarePlus, Plus } from "lucide-react";
+
+export default function AddSubjectModalForm(subject) {
+  const {
+    subjectName,
+    setSubjectName,
+    selectedTeacher,
+    setSelectedTeacher,
+    schedules,
+    handleSaveChanges,
+    addSchedule,
+    removeSchedule,
+    updateSchedule,
+    loading,
+  } = useAddSubjectModal(subject);
+
+  const { checkOverlap } = useCheckOverlap(selectedTeacher);
+
+  const form = useForm({
+    resolver: yupResolver(schema(checkOverlap)),
+    defaultValues: subject.subject, // Set default values here
+  });
+
+  const scrollToEmptyDiv = () => {
+    setTimeout(() => {
+      const emptyDiv = document.getElementById("scrollHere");
+      if (emptyDiv) {
+        emptyDiv.scrollIntoView({ behavior: "smooth" });
+      }
+    }, 100);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSaveChanges)}>
+        <DialogHeader>
+          <DialogTitle>Add Schedule</DialogTitle>
+
+          <DialogDescription className="md:max-w-[80%]">
+            Click the "Add Another Schedule" button below to add another
+            schedule.
+          </DialogDescription>
+        </DialogHeader>
+
+        <main className="my-4 grid h-80 gap-4 overflow-y-auto py-4">
+          <FormField
+            control={form.control}
+            name="subjectName"
+            render={({ field }) => (
+              <FormItem>
+                <section className="grid grid-cols-4 items-center gap-4">
+                  <FormLabel htmlFor="name" className="text-right">
+                    Subject <span className="hidden sm:inline">Name</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled
+                      {...field}
+                      id="name"
+                      className="col-span-3"
+                      placeholder="Enter Subject Name"
+                      value={subjectName}
+                      onChange={(e) => {
+                        field.onChange(e); // This should be sufficient for controlled inputs
+                        setSubjectName(e.target.value);
+                      }}
+                    />
+                  </FormControl>
+                </section>
+                <FormMessage className="text-center" />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="selectedTeacher"
+            render={({ field }) => (
+              <FormItem>
+                <section className="grid grid-cols-4 items-center gap-4">
+                  <FormLabel htmlFor="teacher" className="text-right">
+                    Teacher
+                  </FormLabel>
+                  <FormControl>
+                    <SelectTeacherCombobox
+                      disabled
+                      subject={subject}
+                      field={field}
+                      onSelectTeacher={setSelectedTeacher}
+                    />
+                  </FormControl>
+                </section>
+                <FormMessage className="text-center" />
+              </FormItem>
+            )}
+          />
+
+          {schedules.map((schedule, index) => (
+            <div key={index} className="mt-8 flex flex-col gap-4">
+              {/* {schedules.length > 1 && (
+                <Button
+                  variant="text"
+                  className="col-span-4 text-red-600"
+                  onClick={() => removeSchedule(index)}
+                >
+                  Remove
+                </Button>
+              )} */}
+
+              <FormField
+                control={form.control}
+                name={`schedules[${index}].day`}
+                render={({ field }) => (
+                  <FormItem>
+                    <section className="grid grid-cols-4 items-center gap-4">
+                      <FormLabel
+                        htmlFor={`day-${index}`}
+                        className="text-right"
+                      >
+                        Day
+                      </FormLabel>
+                      <FormControl>
+                        <select
+                          disabled={schedule.subjectId}
+                          {...field}
+                          className="col-span-3 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                          id={`day-${index}`}
+                          value={schedule.day}
+                          onChange={(e) => {
+                            updateSchedule(index, "day", e.target.value);
+                            field.onChange(e); // This should be sufficient for controlled inputs
+                          }}
+                        >
+                          <option value="" disabled>
+                            Select Day
+                          </option>
+                          <option value="Monday">Monday</option>
+                          <option value="Tuesday">Tuesday</option>
+                          <option value="Wednesday">Wednesday</option>
+                          <option value="Thursday">Thursday</option>
+                          <option value="Friday">Friday</option>
+                        </select>
+                      </FormControl>
+                    </section>
+                    <FormMessage className="text-center" />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name={`schedules[${index}].startTime`}
+                render={({ field }) => (
+                  <FormItem>
+                    <section className="grid grid-cols-4 items-center gap-4">
+                      <FormLabel
+                        htmlFor={`startTime-${index}`}
+                        className="text-right"
+                      >
+                        Start Time
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={schedule.subjectId}
+                          {...field}
+                          className="col-span-3"
+                          id={`startTime-${index}`}
+                          type="time"
+                          value={schedule.startTime}
+                          onChange={(e) => {
+                            field.onChange(e); // This should be sufficient for controlled inputs
+                            updateSchedule(index, "startTime", e.target.value);
+                          }}
+                        />
+                      </FormControl>
+                    </section>
+                    <FormMessage className="text-center" />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name={`schedules[${index}].endTime`}
+                render={({ field }) => (
+                  <FormItem>
+                    <section className="grid grid-cols-4 items-center gap-4">
+                      <FormLabel
+                        htmlFor={`endTime-${index}`}
+                        className="text-right"
+                      >
+                        End Time
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={schedule.subjectId}
+                          {...field}
+                          className="col-span-3"
+                          id={`endTime-${index}`}
+                          type="time"
+                          value={schedule.endTime}
+                          onChange={(e) => {
+                            updateSchedule(index, "endTime", e.target.value);
+                            field.onChange(e); // This should be sufficient for controlled inputs
+                          }}
+                        />
+                      </FormControl>
+                    </section>
+                    <FormMessage className="text-center" />
+                  </FormItem>
+                )}
+              />
+            </div>
+          ))}
+          <div id="scrollHere"></div>
+        </main>
+
+        {form.formState.errors.schedules && (
+          <p className="text-center text-red-500">
+            {form.formState.errors.schedules.message}
+          </p>
+        )}
+
+        <DialogFooter>
+          <Button
+            variant="outline"
+            type="button"
+            // onClick={addSchedule}
+            onClick={() => {
+              addSchedule();
+              scrollToEmptyDiv();
+            }}
+            className=""
+          >
+            <BadgePlus className="mr-2 h-4 w-4" /> Add another schedule
+          </Button>
+
+          <Button type="submit" disabled={loading}>
+            Save changes
+          </Button>
+        </DialogFooter>
+      </form>
+    </Form>
+  );
+}
---
+++ src/pages/Teacher/TeacherSpecificClass/partials/AddScheduleModal/index.jsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
+import useAddSubjectModal from "./useAddSubjectModal";
+import AddSubjectModalForm from "./AddSubjectModalForm";
+import { useModal } from "./AddSubjectModal.hooks";
+import { Icon } from "@iconify/react";
+import { TableCell } from "@/components/ui/table";
+
+export default function AddScheduleModal({ subject }) {
+  const { isModalOpen, setIsModalOpen } = useModal();
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DialogTrigger asChild>
+        {/* <TableCell className="inline-block hover:cursor-pointer"> */}
+        {/* <span className="flex items-center gap-2">
+            <span className="border-b border-background py-1 hover:border-foreground">
+              Update
+            </span>
+            <Icon
+              icon="octicon:arrow-down-24"
+              rotate={3}
+              className="ml-2 hidden -rotate-45 transform transition-all group-hover:rotate-45 sm:inline"
+            />
+          </span> */}
+        {/* </TableCell> */}
+
+        <Button size="sm" variant="ghost" className="justify-start px-2">
+          Add schedule
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent className="sm:max-h-[90svh] sm:max-w-[525px]">
+        <AddSubjectModalForm subject={subject} />
+      </DialogContent>
+    </Dialog>
+  );
+}
