3575c0094d06478f88d59fd96089c3b66b58538e
n0wickk <memetitv01@gmail.com>
2024-04-24 08:20:26 +0800

    feat(resetPasswordController): update the reset password function to be universal

--- server/controller/resetPasswordController.js
+++ server/controller/resetPasswordController.js
@@ -1,24 +1,37 @@
 const asyncHandler = require("express-async-handler");
-const ResetPasswordCode = require("../models/ResetPasswordCodeModel");
-const { Student } = require("../models/StudentModel");
 const { transporter } = require("../mailer");
 const bcryptjs = require("bcryptjs");
 
-const resetStudentPassword = asyncHandler(async (req, res) => {
-  const { lrn } = req.body;
+const { Admin } = require("../models/AdminModel");
+const { Teacher } = require("../models/TeacherModel");
+const { Student } = require("../models/StudentModel");
 
+const ResetPasswordCode = require("../models/ResetPasswordCodeModel");
+
+const resetUserPassword = asyncHandler(async (req, res) => {
   try {
-    const student = await Student.findOne({ lrn });
+    let { username } = req.body;
+    username = username.toLowerCase();
 
-    if (!student) {
-      return res
-        .status(404)
-        .json({ message: "Student not found. Please try again." });
+    let user;
+
+    user = await Student.findOne({ lrn: username });
+
+    if (!user) {
+      user = await Teacher.findOne({ username });
     }
 
-    const studentEmail = student.emailAddress;
+    if (!user) {
+      user = await Admin.findOne({ username });
+    }
 
-    if (!studentEmail) {
+    if (!user) {
+      res.status(404).json({ message: "User not found." });
+    }
+
+    const userEmail = user.emailAddress;
+
+    if (!userEmail) {
       return res
         .status(404)
         .json({ message: "Email not found. Please try again." });
@@ -27,26 +40,35 @@ const resetStudentPassword = asyncHandler(async (req, res) => {
     const resetCode = Math.floor(100000 + Math.random() * 900000).toString();
 
     const resetPasswordCode = new ResetPasswordCode({
-      lrn: lrn,
-      email: studentEmail,
+      username,
+      email: userEmail,
       code: resetCode,
     });
 
     const mailOptions = {
-      // from: "noreply@simsv1.com",
       from: {
-        name: "SIMSv1 PASSWORD RESET",
+        name: "SIMSv1 Support",
         address: "noreply@simsv1.com",
       },
-      to: studentEmail,
+      to: userEmail,
       subject: "Password Reset Code",
-      text: `Your password reset code is: ${resetCode}`,
+      html: `
+        <div style="font-family: Arial, sans-serif;">
+          <p>Hello ${user.firstName},</p>
+          <p>We've received a request to reset your password for your SIMSv1 account.</p>
+          <p style="margin-bottom: 20px;">Please use the following code to reset your password:</p>
+          <div style="background-color: #f4f4f4; border: 1px solid #ddd; border-radius: 4px; padding: 10px; margin-bottom: 20px;">
+            <h3 style="margin: 0;">Reset Code:</h3>
+            <p style="font-size: 20px; font-weight: bold; margin: 10px 0;">${resetCode}</p>
+          </div>
+          <p style="margin-bottom: 20px;">If you didn't request this, you can safely ignore this email.</p>
+          <p>Thank you,</p>
+          <p>SIMSv1 Support Team</p>
+        </div>
+      `,
     };
 
     await transporter.sendMail(mailOptions);
-
-    console.log(`studentEmail`, studentEmail);
-    console.log(`resetCode`, resetCode);
 
     await resetPasswordCode.save();
 
@@ -110,4 +132,4 @@ const resetStudentPasswordFinal = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { resetStudentPassword, resetStudentPasswordFinal };
+module.exports = { resetUserPassword, resetStudentPasswordFinal };
