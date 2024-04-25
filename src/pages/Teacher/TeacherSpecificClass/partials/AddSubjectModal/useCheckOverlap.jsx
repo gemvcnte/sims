7e212469088be7fd46a60777af544dbfc053eedb
n0wickk <memetitv01@gmail.com>
2024-04-26 06:52:40 +0800

    fix(addsubject-useCheckOverlap): only add selectedTeacherSchedule if there is a selectedTeacher

--- src/pages/Teacher/TeacherSpecificClass/partials/AddSubjectModal/useCheckOverlap.jsx
+++ src/pages/Teacher/TeacherSpecificClass/partials/AddSubjectModal/useCheckOverlap.jsx
@@ -21,11 +21,17 @@ export const useCheckOverlap = (selectedTeacher) => {
   }, []);
 
   const checkOverlap = (schedules) => {
-    const allSchedules = [
-      ...schedules,
-      ...schedulesFromDb,
-      ...selectedTeacherSchedule,
-    ];
+    let allSchedules = [...schedules, ...schedulesFromDb];
+
+    if (selectedTeacher) {
+      // Convert selectedTeacherSchedule object to array
+      const selectedTeacherScheduleArray = Object.values(
+        selectedTeacherSchedule,
+      ).flatMap((daySchedules) => daySchedules);
+
+      // Spread the selectedTeacherScheduleArray
+      allSchedules = [...allSchedules, ...selectedTeacherScheduleArray];
+    }
 
     for (let i = 0; i < allSchedules.length - 1; i++) {
       for (let j = i + 1; j < allSchedules.length; j++) {
@@ -48,6 +54,8 @@ export const useCheckOverlap = (selectedTeacher) => {
           schedule1.startTime === schedule2.startTime &&
           schedule1.endTime === schedule2.endTime &&
           schedule1.subjectId !== schedule2.subjectId
+          // schedule1.subjectId !== undefined &&
+          // schedule2.subjectId !== undefined
         ) {
           // If schedules are exactly the same, return the overlapping schedule message
           const overlappingSchedule = `${schedule1.day} (${schedule1.startTime} - ${schedule1.endTime}) is the same as ${schedule2.day} (${schedule2.startTime} - ${schedule2.endTime})`;
