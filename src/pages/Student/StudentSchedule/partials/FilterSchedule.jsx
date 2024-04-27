5d180efa51de9634088e2f57d1c214a1771f6af4
ampol <memetitv01@gmail.com>
2024-04-27 13:58:26 +0800

    Fix 441/bug student account undefined values on production (#442)
    * fix(StudentDashboard): remove custom "Welcome, {firstName} nessage"
    
    * fix(user-nav): fix displayText condition
    
    * fix(filterDrawers): change gas to he filter
    
    * fix(FilterSchedule): add validation of .length === 0
    
    * fix(FilterGrades): add .length === 0 validation

--- src/pages/Student/StudentSchedule/partials/FilterSchedule.jsx
+++ src/pages/Student/StudentSchedule/partials/FilterSchedule.jsx
@@ -16,7 +16,12 @@ export default function FilterSchedule() {
   const { schoolYearAndSemesterSelectOptions, filterClassDetails } =
     useClassDetails();
 
-  if (!schoolYearAndSemesterSelectOptions) return null;
+  if (
+    !schoolYearAndSemesterSelectOptions ||
+    schoolYearAndSemesterSelectOptions.length === 0
+  ) {
+    return null;
+  }
 
   const mostLatestSchoolYearAndSemester = `${schoolYearAndSemesterSelectOptions[0].schoolYear}-${schoolYearAndSemesterSelectOptions[0].semester}`;
 
