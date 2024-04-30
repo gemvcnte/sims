35aad4ec83792ec66bbf1282865f782a4583d5f9
n0wickk <memetitv01@gmail.com>
2024-04-30 21:39:31 +0800

    (login): change announcemnet card style

--- src/pages/Login/components/AnnouncementsModal/AnnouncementCard.jsx
+++ src/pages/Login/components/AnnouncementsModal/AnnouncementCard.jsx
@@ -1,15 +1,69 @@
-import React from "react";
-import { Icon } from "@iconify/react";
+import React, { useEffect, useState } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
-const AnnouncementCard = ({
-  announcement,
-  expandedAnnouncement,
-  toggleContent,
-}) => (
-  <div key={announcement._id} className="mb-4 rounded-lg border p-4">
-    <div className="flex items-center justify-between">
-      <div>
-        <h2 className="break-all font-semibold">{announcement.title}</h2>
+const AnnouncementCard = ({ announcement }) => {
+  const [accordionOpen, setAccordionOpen] = useState(false);
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  const checkIsMobile = () => {
+    const width = window.innerWidth;
+    setIsMobile(width <= 333);
+  };
+
+  useEffect(() => {
+    checkIsMobile();
+
+    window.addEventListener("resize", checkIsMobile);
+    return () => {
+      window.removeEventListener("resize", checkIsMobile);
+    };
+  }, []);
+
+  const toggleAccordion = () => {
+    setAccordionOpen(!accordionOpen);
+  };
+
+  const [showFullTitle, setShowFullTitle] = useState(false);
+
+  const toggleTitle = () => {
+    setShowFullTitle(!showFullTitle);
+  };
+
+  return (
+    <div key={announcement._id} className="mb-4 rounded-lg border p-4">
+      <div className="flex flex-col break-all">
+        <section className=" flex items-center justify-between gap-4">
+          {isMobile ? (
+            <h2 className="break-all font-semibold">
+              {showFullTitle ? (
+                announcement.title
+              ) : (
+                <>
+                  {announcement.title.length > 20
+                    ? `${announcement.title.substring(0, 20)}...`
+                    : announcement.title}
+                  {announcement.title.length > 20 && (
+                    <button
+                      className="text-xs italic text-muted-foreground hover:underline sm:ml-2"
+                      onClick={toggleTitle}
+                    >
+                      Show Full Title
+                    </button>
+                  )}
+                </>
+              )}
+            </h2>
+          ) : (
+            <h2 className="break-all font-semibold">{announcement.title}</h2>
+          )}
+          <div></div>
+        </section>
         <p className="text-xs text-muted-foreground">
           {new Date(announcement.createdAt).toLocaleDateString("en-US", {
             year: "numeric",
@@ -18,30 +72,20 @@ const AnnouncementCard = ({
           })}
         </p>
       </div>
+      <section className="flex items-baseline justify-between text-xs">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-3">
+            <AccordionTrigger onClick={toggleAccordion}>
+              {accordionOpen ? "Hide Details" : "Show Details"}{" "}
+            </AccordionTrigger>
+            <AccordionContent className="break-all">
+              {announcement.content}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </section>
     </div>
-    <button
-      className="mt-4 flex justify-center gap-1 text-xs"
-      variant="outline"
-      onClick={() => toggleContent(announcement._id)}
-    >
-      {expandedAnnouncement === announcement._id ? (
-        <>
-          Hide Details
-          <Icon icon="dashicons:arrow-up" rotate={2} />
-        </>
-      ) : (
-        <>
-          Show Details
-          <Icon icon="dashicons:arrow-up" rotate={1} />
-        </>
-      )}
-    </button>
-    {expandedAnnouncement === announcement._id && (
-      <div className="mt-2 break-all text-sm text-gray-400">
-        {announcement.content}
-      </div>
-    )}
-  </div>
-);
+  );
+};
 
 export default AnnouncementCard;
