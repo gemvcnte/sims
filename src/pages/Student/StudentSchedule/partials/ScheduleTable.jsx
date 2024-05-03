96516f41407e0bb7f627a4b4518d26dcfc0a2b1c
n0wickk <memetitv01@gmail.com>
2024-05-03 08:26:42 +0800

    ref(ScheduleTable): refactor student schedule

--- src/pages/Student/StudentSchedule/partials/ScheduleTable.jsx
+++ src/pages/Student/StudentSchedule/partials/ScheduleTable.jsx
@@ -8,83 +8,86 @@ import { Button } from "@/components/ui/button";
 import generatePDF, { Resolution, Margin } from "react-to-pdf";
 import FilterSchedule from "./FilterSchedule";
 import { Download } from "lucide-react";
+import useIsMobile from "@/hooks/useIsMobile";
 
 const localizer = momentLocalizer(moment);
 
-const minTime = new Date();
-minTime.setHours(7, 0, 0); // Minimum time set to 7am
+const minTimeSevenAm = new Date();
+minTimeSevenAm.setHours(7, 0, 0);
 
-const maxTime = new Date();
-maxTime.setHours(18, 0, 0); // Maximum time set to 6pm
+const maxTimeSixPm = new Date();
+maxTimeSixPm.setHours(18, 0, 0);
 
 const ScheduleTable = () => {
   const classDetailsContext = useClassDetails();
   const { classDetails: fetchedClassDetails, loading } = classDetailsContext;
 
-  // State to track device width
-  const [isMobile, setIsMobile] = useState(false);
-
-  const checkIsMobile = () => {
-    const width = window.innerWidth;
-    setIsMobile(width <= 768);
-  };
-
-  useEffect(() => {
-    checkIsMobile();
-  }, []);
+  const isMobile = useIsMobile();
 
   if (loading) {
     return <AdminScheduleSkeleton />;
   }
 
   const generateEvents = () => {
-    const events = [];
-    const firstClassDetail = fetchedClassDetails[0];
+    const generatedEvents = [];
+    const firstFetchedClassDetail =
+      fetchedClassDetails && fetchedClassDetails[0];
 
-    // Check if the first class detail exists and has subjects
-    if (firstClassDetail && firstClassDetail.subjects) {
-      // Loop through subjects array of the first class detail
-      firstClassDetail.subjects.forEach((subject) => {
-        // Loop through schedules array for each subject
-        subject.schedules.forEach((schedule) => {
-          // Check if the schedule day is Monday, Tuesday, Wednesday, Thursday, or Friday
-          if (
-            ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].includes(
-              schedule.day,
-            )
-          ) {
-            // Push event for the subject with appropriate start and end times
-            events.push({
-              id: `${firstClassDetail._id}-${subject._id}-${schedule._id}`,
-              title: isMobile
-                ? abbreviateSubject(subject.subjectName)
-                : subject.subjectName,
-              start: moment()
-                .day(schedule.day)
-                .hour(Number(schedule.startTime.split(":")[0]))
-                .minute(Number(schedule.startTime.split(":")[1]))
-                .toDate(),
-              end: moment()
-                .day(schedule.day)
-                .hour(Number(schedule.endTime.split(":")[0]))
-                .minute(Number(schedule.endTime.split(":")[1]))
-                .toDate(),
-              allDay: false,
-            });
-          }
-        });
-      });
+    if (
+      !firstFetchedClassDetail ||
+      !Array.isArray(firstFetchedClassDetail.subjects)
+    ) {
+      return generatedEvents;
     }
 
-    return events;
+    const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+    firstFetchedClassDetail.subjects.forEach((subject) => {
+      if (!subject || !Array.isArray(subject.schedules)) {
+        return;
+      }
+
+      subject.schedules.forEach((schedule) => {
+        const { day: scheduleDay, startTime, endTime } = schedule || {};
+
+        if (!weekdays.includes(scheduleDay)) {
+          return;
+        }
+
+        const eventStartTime = calculateEventTime(scheduleDay, startTime);
+        const eventEndTime = calculateEventTime(scheduleDay, endTime);
+
+        generatedEvents.push({
+          id: generateEventId(firstFetchedClassDetail, subject, schedule),
+          title: isMobile
+            ? abbreviateSubject(subject.subjectName)
+            : subject.subjectName,
+          start: eventStartTime,
+          end: eventEndTime,
+          allDay: false,
+        });
+      });
+    });
+
+    return generatedEvents;
   };
 
-  // Function to abbreviate subject title
-  const abbreviateSubject = (subjectName) => {
-    // Split subject name by space
-    const words = subjectName.split(" ");
-    // Take the first character of each word and join them
-    return words.map((word) => word[0]?.toUpperCase()).join(".");
+  const calculateEventTime = (day, timeString) => {
+    const [hours, minutes] = timeString.split(":").map(Number);
+    return moment().day(day).hour(hours).minute(minutes).toDate();
+  };
+
+  const generateEventId = (classDetail, subject, schedule) => {
+    return `${classDetail._id}-${subject._id}-${schedule._id}`;
+  };
+
+  const abbreviateSubject = (subjectFullName) => {
+    const wordsInFullName = subjectFullName.split(" ");
+    const abbreviatedLetters = wordsInFullName.map((word) =>
+      word[0]?.toUpperCase(),
+    );
+    const abbreviatedSubject = abbreviatedLetters.join(".");
+    return abbreviatedSubject;
   };
 
   const dayFormat = (date, culture, localizer) =>
@@ -129,16 +132,14 @@ const ScheduleTable = () => {
           localizer={localizer}
           events={generateEvents()}
           formats={{ dayFormat }}
-          min={minTime} // Set minimum time
-          max={maxTime} // Set maximum time
+          min={minTimeSevenAm}
+          max={maxTimeSixPm}
           step={15} // Interval between time slots
           startAccessor="start" // Start time accessor
           endAccessor="end" // End time accessor
           toolbar={false} // Hide the toolbar
           views={{ work_week: true }} // Show only the work_week view
           defaultView="work_week" // Default view set to work_week
-          // view="work_week" // Default view set to work_week
-          // Conditionally set title accessor based on device width
           titleAccessor={isMobile ? "title" : undefined}
         />
       </div>
