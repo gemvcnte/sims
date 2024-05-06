bc1957779a50cefb2d3e4596ec49a4d0825f2f92
n0wickk <memetitv01@gmail.com>
2024-05-06 10:18:12 +0800

    add dashboard analytics context and layout

--- src/pages/Admin/AdminDashboard/index.jsx
+++ src/pages/Admin/AdminDashboard/index.jsx
@@ -3,17 +3,38 @@ import Topbar from "@/components/layout/Topbar";
 import DashboardAnnouncementCard from "./partials/DashboardAnnouncementCard";
 import ProfileProgressCard from "./partials/ProfileProgressCard";
 import { useAdminProfile } from "../AdminProfile/components/AdminProfileDisplayAndEditSection/hooks";
+import { useTeacherAdminMode } from "@/hooks/useTeacherAdminMode";
+import AdminDashboardLayout from "./index.layout";
+import useDashboardAnalytics from "./hooks/useDashboardAnalytics";
 
 export default function AdminDashboard() {
-  const { adminProfile, error } = useAdminProfile();
+  const { dashboardAnalyticsData, loading, error } = useDashboardAnalytics();
+  const { isAdminMode } = useTeacherAdminMode();
+
+  if (loading) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
 
   return (
-    <main className="w-full">
-      <Topbar>DASHBOARD</Topbar>
-      <section className="flex flex-col gap-4 p-4 md:flex-row">
-        <ProfileProgressCard />
-        <DashboardAnnouncementCard />
-      </section>
-    </main>
+    <AdminDashboardLayout>
+      <main className="w-full">
+        <Topbar>DASHBOARD</Topbar>
+
+        {isAdminMode ? (
+          <section className="p-4">
+            <p>{dashboardAnalyticsData.totalStudents}</p>
+          </section>
+        ) : null}
+
+        <section className="flex flex-col gap-4 p-4 md:flex-row">
+          <ProfileProgressCard />
+          <DashboardAnnouncementCard />
+        </section>
+      </main>
+    </AdminDashboardLayout>
   );
 }
