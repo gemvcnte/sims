905e3529b2490ad2e196ab2e5697f0a78c8602d9
n0wickk <memetitv01@gmail.com>
2024-05-09 22:01:42 +0800

    fix(CreateClassAnnouncementItem): use global ButtonDropdownItem

--- src/components/layout/Sidebar/components/AdminSidebarTeacherMode/CreateClassAnnouncementItem.jsx
+++ src/components/layout/Sidebar/components/AdminSidebarTeacherMode/CreateClassAnnouncementItem.jsx
@@ -1,3 +1,4 @@
+import ButtonDropdownItem from "@/components/button-dropdown-item";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import CreateAdminClassAnnouncementModal from "@/pages/Admin/CreateAdminAnnouncementModal";
@@ -15,9 +16,9 @@ export default function CreateClassAnnouncementItem() {
   return (
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <DialogTrigger>
-        <button className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
-          <Link onClick={handleDialogClick}>Create Class Announcement</Link>
-        </button>
+        <Link onClick={handleDialogClick}>
+          <ButtonDropdownItem>Create Class Announcement</ButtonDropdownItem>
+        </Link>
       </DialogTrigger>
       <CreateAdminClassAnnouncementModal onClose={handleDialogClick} />
     </Dialog>
